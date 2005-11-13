a238c916573184a8872d5c5b9cec0d247f4f311d
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2005-11-13 06:32:11 +0000

    added code for fail-over. needs testing

--- src/com/sun/gi/comm/routing/impl/RouterImpl.java
+++ src/com/sun/gi/comm/routing/impl/RouterImpl.java
@@ -2,8 +2,11 @@ package com.sun.gi.comm.routing.impl;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.sun.gi.comm.routing.ChannelID;
 import com.sun.gi.comm.routing.Router;
@@ -15,175 +18,283 @@ import com.sun.gi.framework.interconnect.TransportChannel;
 import com.sun.gi.framework.interconnect.TransportChannelListener;
 import com.sun.gi.framework.interconnect.TransportManager;
 import com.sun.gi.framework.logging.SGSERRORCODES;
+import com.sun.gi.utils.SGSUUID;
+import com.sun.gi.utils.StatisticalUUID;
 import com.sun.gi.utils.types.BYTEARRAY;
 
 public class RouterImpl implements Router {
 
-	private Map<UserID,SGSUser> userMap = new HashMap<UserID,SGSUser>();
-	private TransportManager transportManager;	
-	private TransportChannel routerControlChannel;
-	private Map<ChannelID,SGSChannel> channelMap = new HashMap<ChannelID,SGSChannel>();
-	private Map<String,SGSChannel> channelNameMap = new HashMap<String,SGSChannel>();
-	private Map<UserID,BYTEARRAY> currentKeys = new HashMap<UserID,BYTEARRAY>();
-	private Map<UserID,BYTEARRAY> previousKeys = new HashMap<UserID,BYTEARRAY>();	
-	private ByteBuffer hdr = ByteBuffer.allocate(256);
-	private RouterListener listener;
-	
-	private enum OPCODE {UserJoined,UserLeft,UserJoinedChannel,UserLeftChannel};
-	
+	private Map<UserID, SGSUser> userMap = new HashMap<UserID, SGSUser>();
 
-	public RouterImpl(TransportManager cmgr) throws IOException{
+	private TransportManager transportManager;
+
+	private TransportChannel routerControlChannel;
+
+	private Map<ChannelID, SGSChannel> channelMap = new HashMap<ChannelID, SGSChannel>();
+
+	private Map<String, SGSChannel> channelNameMap = new HashMap<String, SGSChannel>();
+
+	private Map<UserID, BYTEARRAY> currentKeys = new HashMap<UserID, BYTEARRAY>();
+
+	private Map<UserID, BYTEARRAY> previousKeys = new HashMap<UserID, BYTEARRAY>();
+
+	private ByteBuffer hdr = ByteBuffer.allocate(256);
+
+	private RouterListener listener;
+
+	protected int keySecondsToLive;
+
+	private enum OPCODE {
+		UserJoined, UserLeft, UserJoinedChannel, UserLeftChannel, ReconnectKey
+	};
+
+	public RouterImpl(TransportManager cmgr) throws IOException {
 		transportManager = cmgr;
-		
-		routerControlChannel = transportManager.openChannel("__SGS_ROUTER_CONTROL");
+
+		routerControlChannel = transportManager
+				.openChannel("__SGS_ROUTER_CONTROL");
 		routerControlChannel.addListener(new TransportChannelListener() {
 
 			public void dataArrived(ByteBuffer buff) {
-				OPCODE opcode = OPCODE.values()[(int)buff.get()];
-				switch (opcode){
-					case UserJoined:
-						int idlen = buff.getInt();
-						byte[] idbytes = new byte[idlen];
-						buff.get(idbytes);
-						reportUserJoined(idbytes);
-						break;
-					case UserLeft:
+				OPCODE opcode = OPCODE.values()[(int) buff.get()];
+				switch (opcode) {
+				case UserJoined:
+					int idlen = buff.getInt();
+					byte[] idbytes = new byte[idlen];
+					buff.get(idbytes);
+					reportUserJoined(idbytes);
+					break;
+				case UserLeft:
+					idlen = buff.getInt();
+					idbytes = new byte[idlen];
+					buff.get(idbytes);
+					reportUserLeft(idbytes);
+					break;
+				case UserJoinedChannel:
+					// TODO
+					break;
+				case UserLeftChannel:
+					// TODO
+					break;
+				case ReconnectKey:
+					idlen = buff.getInt();
+					idbytes = new byte[idlen];
+					buff.get(idbytes);
+					try {
+						UserID uid = new UserID(idbytes);
 						idlen = buff.getInt();
 						idbytes = new byte[idlen];
 						buff.get(idbytes);
-						reportUserLeft(idbytes);
-						break;
-					case UserJoinedChannel:
-						break;
-					case UserLeftChannel:
-						break;
+						BYTEARRAY ba = new BYTEARRAY(idbytes);
+						synchronized (currentKeys) {
+							currentKeys.put(uid,ba);
+						}
+					} catch (InstantiationException e) {
+						e.printStackTrace();
+					}
+					break;
 				}
-				
-			}
 
-			
+			}
 
 			public void channelClosed() {
-				SGSERRORCODES.FatalErrors.RouterFailure.fail("Router control channel failed.");				
+				SGSERRORCODES.FatalErrors.RouterFailure
+						.fail("Router control channel failed.");
 			}
-			
 		});
+		// initialize key TTL
+		keySecondsToLive = 120; // 2 min lifetime by default
+		String ttlStr = System.getProperty("sgs.router.keyTTL");
+		if (ttlStr != null) {
+			keySecondsToLive = Integer.parseInt(ttlStr);
+		}
+		// key issuance thread
+		new Thread(new Runnable() {
+			public void run() {
+				long lastTicTime = System.currentTimeMillis();
+				while (true) {
+					try {
+						Thread.sleep(keySecondsToLive * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					if (lastTicTime + (keySecondsToLive * 1000) <= System
+							.currentTimeMillis()) {
+						issueNewKeys();
+						lastTicTime = System.currentTimeMillis();
+					}
+				}
+			}
+		}).start();
 	}
-	
+
+	/**
+	 * 
+	 */
+	protected void issueNewKeys() {
+		synchronized (currentKeys) {
+			previousKeys.clear();
+			previousKeys.putAll(currentKeys);
+			currentKeys.clear();
+			for (Entry<UserID, SGSUser> entry : userMap.entrySet()) {
+				UserID uid = entry.getKey();
+				SGSUser user = entry.getValue();
+				SGSUUID key = new StatisticalUUID();
+				BYTEARRAY keybytes = new BYTEARRAY(key.toByteArray());
+				currentKeys.put(uid, keybytes);
+				try {
+					user
+							.reconnectKeyReceived(keybytes.data(),
+									keySecondsToLive);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				fireConnectKey(uid, keybytes);
+
+			}
+		}
+
+	}
+
+	/**
+	 * @param uid
+	 * @param keybytes	 
+	 */
+	private void fireConnectKey(UserID uid, BYTEARRAY key) {
+		byte[] uidbytes = uid.toByteArray();
+		byte[] keybytes = key.data();
+		synchronized (hdr) {
+			hdr.clear();
+			hdr.put((byte) OPCODE.ReconnectKey.ordinal());
+			hdr.putInt(uidbytes.length);
+			hdr.put(uidbytes);
+			hdr.putInt(keybytes.length);
+			hdr.put(keybytes);
+			try {
+				routerControlChannel.sendData(hdr);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 	private void reportUserLeft(byte[] uidbytes) {
-		for(SGSUser user : userMap.values()){
+		for (SGSUser user : userMap.values()) {
 			try {
 				user.userLeftSystem(uidbytes);
 			} catch (IOException e) {
-				System.out.println("Exception sending UserLeft to user id="+user.getUserID());
+				System.out.println("Exception sending UserLeft to user id="
+						+ user.getUserID());
 				e.printStackTrace();
 			}
 		}
 
-		
 	}
 
 	private void reportUserJoined(byte[] uidbytes) {
-		UserID sentID=null;
+		UserID sentID = null;
 		try {
 			sentID = new UserID(uidbytes);
-		} catch (InstantiationException e1) {			
+		} catch (InstantiationException e1) {
 			e1.printStackTrace();
 			return;
 		}
-		for(SGSUser user : userMap.values()){
-			try {		
-				if (!user.getUserID().equals(sentID)){
+		for (SGSUser user : userMap.values()) {
+			try {
+				if (!user.getUserID().equals(sentID)) {
 					user.userJoinedSystem(uidbytes);
 				}
 			} catch (IOException e) {
-				System.out.println("Exception sending UserJOined to user id="+user.getUserID());
+				System.out.println("Exception sending UserJOined to user id="
+						+ user.getUserID());
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	private void reportUserJoinedChannel(byte[] chanID, byte[] uidbytes) {
-		UserID sentID=null;
+		UserID sentID = null;
 		try {
 			sentID = new UserID(uidbytes);
-		} catch (InstantiationException e1) {			
+		} catch (InstantiationException e1) {
 			e1.printStackTrace();
 			return;
 		}
-		for(SGSUser user : userMap.values()){
-			try {		
-				if (!user.getUserID().equals(sentID)){
+		for (SGSUser user : userMap.values()) {
+			try {
+				if (!user.getUserID().equals(sentID)) {
 					user.userJoinedChannel(chanID, uidbytes);
 				}
 			} catch (IOException e) {
-				System.out.println("Exception sending UserJOined to user id="+user.getUserID());
+				System.out.println("Exception sending UserJOined to user id="
+						+ user.getUserID());
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
-	public void registerUser(SGSUser user) 
-		throws InstantiationException, IOException {		
-		userMap.put(user.getUserID(),user);		
+	public void registerUser(SGSUser user) throws InstantiationException,
+			IOException {
+		userMap.put(user.getUserID(), user);
 		fireUserJoined(user.getUserID());
 		reportUserJoined(user.getUserID().toByteArray());
-		// send already connected users to new joiner		
-		for(UserID oldUserID: userMap.keySet()){			
-			if (oldUserID != user.getUserID()){
-				user.userJoinedSystem(oldUserID.toByteArray());					
+		// send already connected users to new joiner
+		for (UserID oldUserID : userMap.keySet()) {
+			if (oldUserID != user.getUserID()) {
+				user.userJoinedSystem(oldUserID.toByteArray());
 			}
-		}		
+		}
 	}
 
 	private void fireUserJoined(UserID uid) {
 		byte[] uidbytes = uid.toByteArray();
-		synchronized(hdr){
+		synchronized (hdr) {
 			hdr.clear();
-			hdr.put((byte)OPCODE.UserJoined.ordinal());
-		
+			hdr.put((byte) OPCODE.UserJoined.ordinal());
+
 			hdr.putInt(uidbytes.length);
 			hdr.put(uidbytes);
 			try {
 				routerControlChannel.sendData(hdr);
-			} catch (IOException e) {				
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	private void fireUserJoinedChannel(ChannelID cid, UserID uid) {
 		byte[] uidbytes = uid.toByteArray();
 		byte[] cidbytes = cid.toByteArray();
-		synchronized(hdr){
+		synchronized (hdr) {
 			hdr.clear();
-			hdr.put((byte)OPCODE.UserJoinedChannel.ordinal());		
+			hdr.put((byte) OPCODE.UserJoinedChannel.ordinal());
 			hdr.putInt(uidbytes.length);
 			hdr.put(uidbytes);
 			hdr.putInt(cidbytes.length);
 			hdr.put(cidbytes);
 			try {
 				routerControlChannel.sendData(hdr);
-			} catch (IOException e) {				
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	public void deregisterUser(SGSUser user) {
 		UserID id = user.getUserID();
 		userMap.remove(id);
-		for (SGSChannel chan : channelMap.values()){
+		for (SGSChannel chan : channelMap.values()) {
 			chan.leave(user);
 		}
 		user.deregistered();
 		fireUserLeft(id);
-		for(SGSUser localUser : userMap.values()){
-			if (localUser != user){
+		for (SGSUser localUser : userMap.values()) {
+			if (localUser != user) {
 				try {
 					localUser.userLeftSystem(id.toByteArray());
 				} catch (IOException e) {
@@ -196,68 +307,68 @@ public class RouterImpl implements Router {
 
 	private void fireUserLeft(UserID uid) {
 		byte[] uidbytes = uid.toByteArray();
-		synchronized(hdr){
+		synchronized (hdr) {
 			hdr.clear();
-			hdr.put((byte)OPCODE.UserLeft.ordinal());		
+			hdr.put((byte) OPCODE.UserLeft.ordinal());
 			hdr.putInt(uidbytes.length);
 			hdr.put(uidbytes);
 			try {
 				routerControlChannel.sendData(hdr);
-			} catch (IOException e) {				
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	public SGSChannel openChannel(String channelName) {
 		SGSChannel sgschan = channelNameMap.get(channelName);
-		if (sgschan == null){
+		if (sgschan == null) {
 			TransportChannel tchan;
 			try {
 				tchan = transportManager.openChannel(channelName);
-			} catch (IOException e) {				
+			} catch (IOException e) {
 				e.printStackTrace();
 				return null;
 			}
 			try {
-				sgschan = new ChannelImpl(this,tchan);
-				channelMap.put(sgschan.channelID(),sgschan);
-				channelNameMap.put(channelName,sgschan);				
+				sgschan = new ChannelImpl(this, tchan);
+				channelMap.put(sgschan.channelID(), sgschan);
+				channelNameMap.put(channelName, sgschan);
 			} catch (IOException e) {
 				e.printStackTrace();
-			}		
-		}		
+			}
+		}
 		return sgschan;
 	}
-	
-	protected void closeChannel(ChannelImpl channel){
+
+	protected void closeChannel(ChannelImpl channel) {
 		channelNameMap.remove(channel.getName());
 		channelMap.remove(channel.channelID());
 	}
-	
 
-	public boolean validateReconnectKey(UserID uid, byte[] key ){
-		BYTEARRAY currentKey = currentKeys.get(uid);
-		if (currentKey.equals(key)){
-			return true;
-		} 
-		BYTEARRAY pastKey = previousKeys.get(uid);
-		if (pastKey.equals(key)){
-			return true;
-		} 
-		return false;
+	public boolean validateReconnectKey(UserID uid, byte[] key) {
+		synchronized (currentKeys) {
+			BYTEARRAY currentKey = currentKeys.get(uid);
+			if (currentKey.equals(key)) {
+				return true;
+			}
+			BYTEARRAY pastKey = previousKeys.get(uid);
+			if (pastKey.equals(key)) {
+				return true;
+			}
+			return false;
+		}
 	}
-	
-	public void setRouterListener(RouterListener l){
+
+	public void setRouterListener(RouterListener l) {
 		listener = l;
 	}
 
-	public void serverMessage(boolean reliable, UserID userID, ByteBuffer databuff) {
-		listener.serverMessage(userID,databuff,reliable);
-		
-	}
+	public void serverMessage(boolean reliable, UserID userID,
+			ByteBuffer databuff) {
+		listener.serverMessage(userID, databuff, reliable);
 
-	
+	}
 
 }
