18e750e93e7ac5b041889765f821441e8ebc62b3
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2005-11-23 00:45:37 +0000

    adding InstallationURL which was never comitted to old SVN

---
+++ src/com/sun/gi/framework/install/impl/InstallationURL.java
@@ -0,0 +1,110 @@
+package com.sun.gi.framework.install.impl;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.StringTokenizer;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Collection;
+
+import com.sun.gi.framework.install.DeploymentRec;
+import com.sun.gi.framework.install.InstallationLoader;
+import com.sun.gi.framework.install.ValidatorRec;
+import com.sun.gi.framework.install.UserMgrRec;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2004
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class InstallationURL implements InstallationLoader {
+	private Map<Integer, DeploymentRec> idToDeploymentRec = new HashMap<Integer, DeploymentRec>();
+
+	public InstallationURL() {
+	}
+
+	/**
+	 * InstallationFile
+	 * 
+	 * @param file
+	 *            File
+	 */
+	public InstallationURL(URL url) throws InstantiationException,
+			FileNotFoundException, IOException {
+		URLConnection conn = url.openConnection();
+		conn.connect();
+		InputStream content = conn.getInputStream();
+		InputStreamReader isrdr = new InputStreamReader(content);
+		BufferedReader rdr = new BufferedReader(isrdr);
+		String inline = rdr.readLine();
+		URLDeploymentReader deprdr = new URLDeploymentReader();
+		while (inline != null) {
+			if ((inline.length() > 0) && (inline.charAt(0) != '#')) {
+				StringTokenizer tok = new StringTokenizer(inline);
+				int appID = Integer.parseInt(tok.nextToken());
+				String urlname = tok.nextToken();
+				DeploymentRec drec = deprdr.getDeploymentRec(new URL(urlname));
+				if (drec != null) {
+					drec.setID(appID);
+					idToDeploymentRec.put(appID, drec);
+				}
+			}
+			inline = rdr.readLine();
+		}
+	}
+
+	/**
+	 * listGames
+	 * 
+	 * @return List
+	 */
+	public Collection<DeploymentRec> listGames() {
+		return idToDeploymentRec.values();
+	}
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			args = new String[] { "file:Install.txt" };
+		}
+		try {
+			InstallationLoader inst = new InstallationURL(new URL(args[0]));
+			for (DeploymentRec game : inst.listGames()) {
+				System.out.println("Game: " + game.getName());
+				for (UserMgrRec mgr : game.getUserManagers()) {
+					System.out.println("    User Manager:"
+							+ mgr.getServerClassName());
+					for (ValidatorRec mod : mgr.getValidatorModules()) {
+						System.out.println("        Login Module: "
+								+ mod.getValidatorClassName());
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
