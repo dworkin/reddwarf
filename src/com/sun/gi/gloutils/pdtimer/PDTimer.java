4c22a922caeb58671ad5fb7958a118fbbad1fc6a
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2005-12-16 03:53:10 +0000

    comitting PDTimer

---
+++ src/com/sun/gi/gloutils/pdtimer/PDTimer.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * <p>Title: PDTimer.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2004 Sun Microsystems, Inc.</p>
+ * <p>Company: Sun Microsystems, Inc</p>
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+package com.sun.gi.gloutils.pdtimer;
+
+import com.sun.gi.logic.SimTask;
+import com.sun.gi.logic.SimTimerListener;
+
+/**
+ *
+ * <p>Title: PDTimer.java</p>
+ * <p>Description: This is the primary GLO in the Persistant/Distributed timer system</p>
+ * <p>The iniate slice timer is non-persistant and local to the slice.  The PD timer
+ * system is a set of GLOs that are driven off the slice timer manager but provide
+ * for the processing of any event on any slice, and the persistance of registered
+ * events between runs of the slice.
+ * <p>Copyright: Copyright (c) 2004 Sun Microsystems, Inc.</p>
+ * <p>Company: Sun Microsystems, Inc</p>
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+public class PDTimer implements SimTimerListener {
+	
+	public void start(SimTask task,long heartbeat){
+		try {
+			task.registerTimerEvent(heartbeat*1000,true,task.makeReference(this));
+		} catch (InstantiationException e) {			
+			e.printStackTrace();
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.sun.gi.logic.SimTimerListener#timerEvent(com.sun.gi.logic.SimTask, long)
+	 */
+	public void timerEvent(SimTask task, long eventID) {
+		System.out.println("pd timer tick");
+		
+	}
+
+	
+
+}
