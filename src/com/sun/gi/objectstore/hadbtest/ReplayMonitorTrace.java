b81c36aa17a20475800fdcd4261b1029e85243d4
de153050 <de153050@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-01-31 10:06:28 +0000

    Draft (in-progress) -DJE

---
+++ src/com/sun/gi/objectstore/hadbtest/ReplayMonitorTrace.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2006, Sun Microsystems, Inc. All Rights Reserved. 
+ */
+
+package com.sun.gi.objectstore.hadbtest;
+
+import com.sun.gi.objectstore.DeadlockException;
+import com.sun.gi.objectstore.NonExistantObjectIDException;
+import com.sun.gi.objectstore.ObjectStore;
+import com.sun.gi.objectstore.Transaction;
+import com.sun.gi.objectstore.tso.TSOObjectStore;
+import com.sun.gi.objectstore.tso.dataspace.DataSpace;
+import com.sun.gi.objectstore.tso.dataspace.InMemoryDataSpace;
+import com.sun.gi.objectstore.tso.dataspace.PersistantInMemoryDataSpace;
+import com.sun.gi.objectstore.tso.dataspace.HadbDataSpace;
+import com.sun.gi.objectstore.tso.dataspace.MonitoredDataSpace;
+import java.io.Serializable;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
+/**
+ * @author Daniel Ellard
+ */
+
+public class ReplayMonitorTrace {
+
+    public static void main(String[] args) throws IOException {
+	String traceFile = args[0];
+	String dspaceType = args[1];
+
+	FileInputStream fis;
+	ObjectInputStream ois;
+
+	try {
+	    fis = new FileInputStream(traceFile);
+	    ois = new ObjectInputStream(fis);
+	} catch (IOException e) {
+	    System.out.println(e);
+	    return;
+	}
+
+	// ObjectStore ostore = connect(true, dspaceType, null);
+
+	for (;;) {
+	    Object o;
+	    try {
+		o = ois.readObject();
+	    } catch (Exception e) {
+		System.out.println(e);
+		break;
+	    }
+
+	    System.out.println(o.getClass().getName());
+	}
+    }
+}
+
