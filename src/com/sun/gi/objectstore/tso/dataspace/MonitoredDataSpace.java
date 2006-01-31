e671ebd01c93070e185f4fe4bd790e14a274420f
de153050 <de153050@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-01-31 20:44:19 +0000

    Cleaning up and adding some doc.
    This wrapper should be completely functional now -- although
    does I/O in a stupid/slow way that will probably need to be
    revisited.
    -DJE

--- src/com/sun/gi/objectstore/tso/dataspace/MonitoredDataSpace.java
+++ src/com/sun/gi/objectstore/tso/dataspace/MonitoredDataSpace.java
@@ -2,31 +2,40 @@
  */
 package com.sun.gi.objectstore.tso.dataspace;
 
+import com.sun.gi.objectstore.NonExistantObjectIDException;
+import com.sun.gi.objectstore.tso.dataspace.monitor.AtomicUpdateTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.ClearTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.CloseTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.GetAppIdTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.GetNextIdTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.GetObjBytesTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.LockTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.LookupTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.ReleaseTraceRecord;
+import com.sun.gi.objectstore.tso.dataspace.monitor.TraceRecord;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.Set;
 
-import com.sun.gi.objectstore.NonExistantObjectIDException;
-import com.sun.gi.objectstore.tso.dataspace.monitor.AtomicUpdateLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.ClearLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.CloseLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.GetAppIdLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.GetNextIdLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.GetObjBytesLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.LockLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.LogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.LookupLogEntry;
-import com.sun.gi.objectstore.tso.dataspace.monitor.ReleaseLogEntry;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.IOException;
-
 /**
+ * Simple wrapper for a {@link DataSpace} implementation that gathers
+ * a trace of its operation.
  */
 public class MonitoredDataSpace implements DataSpace {
     private final DataSpace dataSpace;
-    private boolean loggingEnabled = false;
-    private ObjectOutputStream entryLog;
+    private volatile boolean loggingEnabled = false;
+    private volatile ObjectOutputStream entryLog;
 
+    /**
+     * Simple, degenerate constructor that creates a wrapper that
+     * does <em>not</em> gather any traces.
+     *
+     * @param dataSpace the wrapped {@link DataSpace}
+     *
+     * @throws NullPointerException if dataSpace is <code>null</code>
+     */
     public MonitoredDataSpace(DataSpace dataSpace) {
 	if (dataSpace == null) {
 	    throw new NullPointerException("dataSpace is null");
@@ -37,7 +46,22 @@ public class MonitoredDataSpace implements DataSpace {
 	this.entryLog = null;
     }
 
-    public MonitoredDataSpace(DataSpace dataSpace, String pathName)
+    /**
+     * Creates a wrapper that captures traces of the operations
+     * performed by a {@link DataSpace}. <p>
+     *
+     * If the trace file already exists, it is truncated and overwritten.
+     *
+     * @param dataSpace the {@link DataSpace} to trace
+     *
+     * @param traceFileName the path to the file in which to store the
+     * trace
+     *
+     * @throws NullPointerException if dataSpace is <code>null</code>
+     *
+     * @throws IOException if there is an IO error with the trace file
+     */
+    public MonitoredDataSpace(DataSpace dataSpace, String traceFileName)
 	    throws IOException
     {
 	if (dataSpace == null) {
@@ -46,7 +70,7 @@ public class MonitoredDataSpace implements DataSpace {
 
 	this.dataSpace = dataSpace;
 
-	FileOutputStream fos = new FileOutputStream(pathName, true);
+	FileOutputStream fos = new FileOutputStream(pathName);
 	ObjectOutputStream oos = new ObjectOutputStream(fos);
 
 	this.entryLog = oos;
@@ -57,12 +81,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public long getNextID() {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	long id = dataSpace.getNextID();
 
-	if (loggingEnabled) {
-	    log(new GetNextIdLogEntry(startTime, id));
+	if (loggingEnabled()) {
+	    log(new GetNextIdTraceRecord(startTime, id));
 	}
 
 	return id;
@@ -72,12 +96,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public byte[] getObjBytes(long objectID) {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	byte[] bytes = dataSpace.getObjBytes(objectID);
 
-	if (loggingEnabled) {
-	    log(new GetObjBytesLogEntry(startTime, objectID, bytes.length));
+	if (loggingEnabled()) {
+	    log(new GetObjBytesTraceRecord(startTime, objectID, bytes.length));
 	}
 
 	return bytes;
@@ -89,12 +113,12 @@ public class MonitoredDataSpace implements DataSpace {
     public void lock(long objectID)
 	    throws NonExistantObjectIDException
     {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	dataSpace.lock(objectID);
 
-	if (loggingEnabled) {
-	    log(new LockLogEntry(startTime, objectID));
+	if (loggingEnabled()) {
+	    log(new LockTraceRecord(startTime, objectID));
 	}
     }
 
@@ -102,12 +126,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public void release(long objectID) {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	dataSpace.release(objectID);
 
-	if (loggingEnabled) {
-	    log(new ReleaseLogEntry(startTime, objectID));
+	if (loggingEnabled()) {
+	    log(new ReleaseTraceRecord(startTime, objectID));
 	}
     }
 
@@ -119,13 +143,13 @@ public class MonitoredDataSpace implements DataSpace {
 	    Map<Long, byte[]> updateMap, Set<Long> insertSet)
 	throws DataSpaceClosedException
     {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	dataSpace.atomicUpdate(clear, newNames,
 		deleteSet, updateMap, insertSet);
 
-	if (loggingEnabled) {
-	    log(new AtomicUpdateLogEntry(startTime, clear, newNames,
+	if (loggingEnabled()) {
+	    log(new AtomicUpdateTraceRecord(startTime, clear, newNames,
 		    deleteSet, updateMap, insertSet));
 	}
     }
@@ -134,12 +158,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public Long lookup(String name) {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	Long oid = dataSpace.lookup(name);
 
-	if (loggingEnabled) {
-	    log(new LookupLogEntry(startTime, name, oid));
+	if (loggingEnabled()) {
+	    log(new LookupTraceRecord(startTime, name, oid));
 	}
 
 	return oid;
@@ -149,12 +173,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public long getAppID() {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	long appId = dataSpace.getAppID();
 
-	if (loggingEnabled) {
-	    log(new GetAppIdLogEntry(startTime, appId));
+	if (loggingEnabled()) {
+	    log(new GetAppIdTraceRecord(startTime, appId));
 	}
 
 	return appId;
@@ -164,12 +188,12 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public void clear() {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	dataSpace.clear();
 
-	if (loggingEnabled) {
-	    log(new ClearLogEntry(startTime));
+	if (loggingEnabled()) {
+	    log(new ClearTraceRecord(startTime));
 	}
     }
 
@@ -177,25 +201,60 @@ public class MonitoredDataSpace implements DataSpace {
      * {@inheritDoc}
      */
     public void close() {
-	long startTime = loggingEnabled ? System.currentTimeMillis() : -1;
+	long startTime = loggingEnabled() ? System.currentTimeMillis() : -1;
 
 	dataSpace.close();
 
-	if (loggingEnabled) {
-	    log(new CloseLogEntry(startTime));
+	if (loggingEnabled()) {
+	    log(new CloseTraceRecord(startTime));
+	    closeLog();
 	}
     }
 
-    public void loggingEnabled(boolean val) {
+    /**
+     * Enables/disables logging.
+     *
+     * @param val enable logging if <code>true</code>, disable
+     * logging if <code>false</code>
+     */
+    public void enableLogging(boolean val) {
 	loggingEnabled = val;
     }
 
+    /**
+     * Indicates whether logging is currently enabled.
+     *
+     * @return <code>true<code> if logging is enabled, <code>false</code
+     * otherwise
+     */
     public boolean loggingEnabled() {
 	return loggingEnabled;
     }
 
-    private synchronized void log(LogEntry entry) {
-	if (loggingEnabled && (entryLog != null)) {
+    private synchronized void closeLog() {
+
+	if (!loggingEnabled() || (entryLog == null)) {
+	    return;
+	}
+
+	try {
+	    entryLog.flush();
+	    entryLog.close();
+	} catch (IOException e) {
+	    // XXX: should do something intelligent.  Doesn't.
+	}
+
+	entryLog = null;
+	enableLogging(false);
+    }
+
+    /**
+     * Logs a {@link TraceRecord} to the trace file.
+     *
+     * @param entry the {@link TraceRecord} to store
+     */
+    private synchronized void log(TraceRecord entry) {
+	if (loggingEnabled() && (entryLog != null)) {
 	    try {
 		entryLog.writeObject(entry);
 		entryLog.flush();
@@ -210,7 +269,7 @@ public class MonitoredDataSpace implements DataSpace {
 		}
 
 		entryLog = null;
-		loggingEnabled = false;
+		enableLogging(false);
 	    }
 	}
     }
