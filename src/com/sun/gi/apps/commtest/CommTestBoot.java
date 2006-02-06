24d75cc83647260a6cd693bbb3766fca503a8b63
jmegq <jmegq@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-02-06 23:32:24 +0000

    minor formatting

--- src/com/sun/gi/apps/commtest/CommTestBoot.java
+++ src/com/sun/gi/apps/commtest/CommTestBoot.java
@@ -15,123 +15,108 @@ import java.util.ArrayList;
 
 import javax.security.auth.Subject;
 
-public class CommTestBoot implements SimBoot, SimUserListener,
-		SimUserDataListener, SimChannelListener {
-	private static final long serialVersionUID = -560245896319031239L; // turn
-																		// off
-																		// version
-																		// checking
+public class CommTestBoot
+	implements SimBoot, SimUserListener,
+		   SimUserDataListener, SimChannelListener {
 
-	UserID myUserID = null;
+    private static long serialVersionUID = -560245896319031239L;
+    // turn off version checking
 
-	List<UserID> users = new ArrayList<UserID>();
-	ChannelID echoID;
-	ChannelID noJoinID;
+    UserID myUserID = null;
 
-	GLOReference thisobj;
+    List<UserID> users = new ArrayList<UserID>();
+    ChannelID echoID;
+    ChannelID noJoinID;
 
-	/**
-	 * boot
-	 * 
-	 * @param task
-	 *            SimTask
-	 */
-	public void boot(SimTask task, boolean firstBoot) {
-		System.out.println("Booting comm test, appid = " + task.getAppID());
-		thisobj = task.findSO("BOOT");
-		task.addUserListener(thisobj);
-		ChannelID cid = task.openChannel("echo");
-		task.addChannelListener(cid,thisobj);
-		
-		noJoinID = task.openChannel("noJoin");
-		task.lock(noJoinID, true);
-		task.addChannelListener(noJoinID, thisobj);
-		
-		echoID = cid;
+    GLOReference thisobj;
+
+    public void boot(SimTask task, boolean firstBoot) {
+	System.err.println("Booting comm test, appid = " + task.getAppID());
+	thisobj = task.findSO("BOOT");
+	task.addUserListener(thisobj);
+	ChannelID cid = task.openChannel("echo");
+	task.addChannelListener(cid,thisobj);
+
+	noJoinID = task.openChannel("noJoin");
+	task.lock(noJoinID, true);
+	task.addChannelListener(noJoinID, thisobj);
+
+	echoID = cid;
+    }
+
+    public void userLeft(SimTask task, UserID uid) {
+	System.err.println("User left server: " + uid);
+	users.remove(uid);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.logic.SimUserListener#userJoined
+     */
+    public void userJoined(SimTask task, UserID uid, Subject subject) {
+	System.err.print("User Joined server: " + uid + " ( ");
+	for (Object cred : subject.getPublicCredentials()) {
+	    System.err.print(cred + " ");
 	}
+	System.err.println(")");
+	users.add(uid);
+	task.addUserDataListener(uid, thisobj);
 
-	/**
-	 * userLeft
-	 * 
-	 * @param uid
-	 *            UserID
-	 */
-	public void userLeft(SimTask task, UserID uid) {
-		System.out.println("User left server: " + uid);
-		users.remove(uid);
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sun.gi.logic.SimUserListener#userJoined(com.sun.gi.logic.SimTask,
-	 *      com.sun.gi.comm.routing.UserID, javax.security.auth.Subject)
-	 */
-	public void userJoined(SimTask task, UserID uid, Subject subject) {
-		System.out.print("User Joined server: " + uid + " ( ");
-		for (Object cred : subject.getPublicCredentials()) {
-			System.out.print(cred + " ");
-		}
-		System.out.println(")");
-		users.add(uid);
-		task.addUserDataListener(uid, thisobj);
-		
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.logic.SimUserDataListener#userDataReceived
+     */
+    public void userDataReceived(SimTask task, UserID from, ByteBuffer data) {
+	System.err.println("Data from user " + from + ": "
+		+ new String(data.array(),data.arrayOffset(),data.limit()));
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sun.gi.logic.SimUserDataListener#userDataReceived(com.sun.gi.logic.SimTask,
-	 *      com.sun.gi.comm.routing.UserID, com.sun.gi.comm.routing.UserID,
-	 *      com.sun.corba.se.impl.ior.ByteBuffer)
-	 */
-	public void userDataReceived(SimTask task, UserID from, ByteBuffer data) {
-		System.out.println("Data from user " + from + ": "
-				+ new String(data.array(),data.arrayOffset(),data.limit()));
-		
-		// Sten - new router feature test
-		//task.leave(from, echoID);
-		//task.join(from, noJoinID);
-		//task.lock(noJoinID, false);
-	}
+	// Sten - new router feature test
+	//task.leave(from, echoID);
+	//task.join(from, noJoinID);
+	//task.lock(noJoinID, false);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sun.gi.logic.SimUserDataListener#userJoinedChannel(com.sun.gi.comm.routing.ChannelID,
-	 *      com.sun.gi.comm.routing.UserID)
-	 */
-	public void userJoinedChannel(SimTask task, ChannelID cid, UserID uid) {
-		System.out.println("User " + cid + " joined channel " + uid);
-		
-		// test for forcabley closing a channel
-		//task.closeChannel(cid);
-		
-		// test lock from leaving.
-		/*if (cid.equals(noJoinID)) {
-			System.out.println("Locking noJoin");
-			task.lock(noJoinID, true);
-		}*/
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.logic.SimUserDataListener#userJoinedChannel
+     */
+    public void userJoinedChannel(SimTask task, ChannelID cid, UserID uid) {
+	System.err.println("User " + cid + " joined channel " + uid);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sun.gi.logic.SimUserDataListener#userLeftChannel(com.sun.gi.comm.routing.ChannelID,
-	 *      com.sun.gi.comm.routing.UserID)
-	 */
-	public void userLeftChannel(SimTask task, ChannelID cid, UserID uid) {
-		System.out.println("User " + cid + " left channel " + uid);
+	// test for forcabley closing a channel
+	//task.closeChannel(cid);
 
-	}
+	// test lock from leaving.
+	/*if (cid.equals(noJoinID)) {
+	  System.err.println("Locking noJoin");
+	  task.lock(noJoinID, true);
+	  }*/
+    }
 
-	/* (non-Javadoc)
-	 * @see com.sun.gi.logic.SimChannelListener#dataArrived(com.sun.gi.comm.routing.UserID, java.nio.ByteBuffer)
-	 */
-	public void dataArrived(SimTask task, ChannelID cid, UserID from, ByteBuffer buff) {
-		System.out.println("Echoing: " +new String(buff.array(),buff.arrayOffset(),
-				buff.limit()));
-		task.sendData(cid,new UserID[] {from},buff,true);
-		
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.logic.SimUserDataListener#userLeftChannel
+     */
+    public void userLeftChannel(SimTask task, ChannelID cid, UserID uid) {
+	System.err.println("User " + cid + " left channel " + uid);
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.sun.gi.logic.SimChannelListener#dataArrived
+     */
+    public void dataArrived(SimTask task, ChannelID cid,
+	    UserID from, ByteBuffer buff) {
+
+	System.err.println("Echoing: " +
+	    new String(buff.array(),buff.arrayOffset(), buff.limit()));
+
+	task.sendData(cid,new UserID[] {from},buff,true);
+    }
 }
--- src/com/sun/gi/apps/trivial/TrivialBoot.java
+++ src/com/sun/gi/apps/trivial/TrivialBoot.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Method;
 
 import com.sun.gi.logic.SimBoot;
 import com.sun.gi.logic.SimTask;
-import com.sun.gi.logic.SimTask.ACCESS_TYPE;
 
 /**
  * <p>Title: </p>
@@ -16,44 +15,44 @@ import com.sun.gi.logic.SimTask.ACCESS_TYPE;
  */
 
 public class TrivialBoot implements SimBoot {
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private static final boolean TEST_GLO_PARAM=false;
-	private int count=1;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final boolean TEST_GLO_PARAM = false;
+    private int count = 1;
 
-	public TrivialBoot() {
+    public TrivialBoot() {
+    }
+
+    public void boot(SimTask task, boolean firstBoot) {
+	if (firstBoot){
+	    System.out.println("First boot of trivial test");
 	}
 
-	public void boot(SimTask task,boolean firstBoot) {
-		if (firstBoot){
-			System.out.println("First boot of trivial test");
-		}
-		System.out.println("Ran TrivialBoot.boot "+(count++)+ " times");
-		if (TEST_GLO_PARAM){
-			System.out.println("Testing queue of a GLO as a task parmaq (should throw exception");
-			Method m;
-			try {
-				m = getClass().getMethod("illegalTask",new Class[]{SimTask.class,
-					TrivialBoot.class});
-				task.queueTask(task.makeReference(this),m,new Object[] {this});
-			} catch (SecurityException e) {
-				
-				e.printStackTrace();
-			} catch (NoSuchMethodException e) {
-				
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				
-				e.printStackTrace();
-			}
-			
-		}		
-	}
-	
-	public void illegalTask(SimTask task, TrivialBoot boot){
-		System.err.println("Should not be here");
-	}
+	System.out.println("Ran TrivialBoot.boot " + (count++) + " times");
 
+	if (TEST_GLO_PARAM) {
+	    System.out.println("Testing queue of a GLO as a task parmaq" +
+		" (should throw exception)");
+	    Method m;
+	    try {
+		m = getClass().getMethod("illegalTask",
+			new Class[] { SimTask.class, TrivialBoot.class });
+		task.queueTask(task.makeReference(this),
+		    m, new Object[] { this });
+	    } catch (SecurityException e) {
+		e.printStackTrace();
+	    } catch (NoSuchMethodException e) {
+		e.printStackTrace();
+	    } catch (InstantiationException e) {
+		e.printStackTrace();
+	    }
+
+	}		
+    }
+
+    public void illegalTask(SimTask task, TrivialBoot boot) {
+	System.err.println("Should not be here");
+    }
 }
