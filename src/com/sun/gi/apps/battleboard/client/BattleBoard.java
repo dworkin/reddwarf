c2da28664dd52e7d22f64057b81e7dcada312217
jmegq <jmegq@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-02-14 07:01:58 +0000

    Some minor changes and comments

--- src/com/sun/gi/apps/battleboard/client/BattleBoard.java
+++ src/com/sun/gi/apps/battleboard/client/BattleBoard.java
@@ -1,71 +1,135 @@
+/*
+ * Copyright 2006 by Sun Microsystems, Inc.  All rights reserved.
+ */
+
 package com.sun.gi.apps.battleboard.client;
 
 public class BattleBoard {
 
-    /*
-     * Default size, for now.  Use of defaults will be deprecated
-     * later.
-     */
-
     protected final int boardHeight;
     protected final int boardWidth;
     protected final int board[][];
-
     protected int cityCount;
 
     /*
-     * Could use an enum, but that complicates the wire protocol.  So
-     * I'm using old-fashioned hard-wired constants here.
+     * Java purists would urge the use of a Java 1.5 enum here, but to
+     * simplify the text-based protocol, we are using old-fashioned
+     * constants.
+     *
+     * The POS_* constants are used to denote the contents of the
+     * board (either known, or inferred).
      */
 
+    /** Indicates an empty (unoccupied, unbombed) board position. */
     public static final int POS_VACANT	= 0;
-    public static final int POS_CITY	= 1;
-    public static final int POS_NUKED	= 2;
 
+    /** Indicates a board position that is occupied by a city. */
+    public static final int POS_CITY	= 1;
+
+    /** Indicates a board position that has been bombed. */
+    public static final int POS_BOMBED	= 2;
+
+    /** Indicates a board position whose contents are unknown. */
     public static final int POS_UNKN	= 3;
+
+    /** Indicates a board position that is near (adjacent) to a city. */
     public static final int POS_NEAR	= 4;
+
+    /** Indicates a board position that is not near (adjacent) to a city. */
     public static final int POS_MISS	= 5;
 
+    /*
+     * Return codes from bombBoardPosition.
+     */
+
+    /** Indicates the bomb missed any city. */
     static final int MISS       = 100;
+
+    /** Indicates that the bomb landed adjacent to a city. */
     static final int NEAR_MISS  = 101;
+
+    /** Indicates that the bomb hit a city. */
     static final int HIT        = 102;
 
-    public BattleBoard(int width, int height) {
+    /**
+     * Creates a BattleBoard with the given width and height and
+     * initializes it with cities.  <p>
+     *
+     * For the sake of simplicity, this constructor always places the
+     * cities in the same pattern.  This makes the game very boring to
+     * play, but makes the example somewhat simpler.
+     *
+     * @param width the width of the board
+     *
+     * @param height the height of the board
+     *
+     * @param numCities the number of cities to place on the board
+     *
+     * @throws IllegalArgumentException if the board is an invalid
+     * size (either width or height less than 1), if the number of
+     * cities is less than one, or if the number of cities is more
+     * than will fit onto the board
+     */
+    public BattleBoard(int width, int height, int numCities) {
 
 	if ((width <= 0) || (height <= 0)) {
-	    throw new IllegalArgumentException("invalid board size");
+	    throw new IllegalArgumentException("width and height must be > 0");
+	}
+
+	if (numCities < 1) {
+	    throw new IllegalArgumentException("numCities must be > 0");
+	}
+
+	if (numCities > (width * height)) {
+	    throw new IllegalArgumentException("numCities is too large");
 	}
 
 	boardWidth = width;
 	boardHeight = height;
-
-	cityCount = (width < height) ? width : height;
-
+	cityCount = numCities;
 	board = new int[boardWidth][boardHeight];
 
-	for (int i = 0; i < boardWidth; i++) {
-	    for (int j = 0; j < boardHeight; j++) {
-		board[i][j] = POS_VACANT;
+	for (int x = 0; x < boardWidth; x++) {
+	    for (int y = 0; y < boardHeight; y++) {
+		board[x][y] = POS_VACANT;
 	    }
 	}
 
-	// XXX This is completely lame.  I'm just going to put the
-	// cities in the same place, every time, for debugging. 
-	// -DJE
+	/*
+	 * Note that the city locations are chosen in a completely
+	 * non-random manner...
+	 */
 
-	for (int i = 0; i < cityCount; i++) {
-	    board[i][i] = POS_CITY;
+	int count = numCities;
+	for (int y = 0; (y < boardHeight) && (count > 0); y++) {
+	    for (int x = 0; (x < boardWidth) && (count > 0); x++) {
+		board[x][y] = POS_CITY;
+		count--;
+	    }
 	}
     }
 
+    /**
+     * Returns the height of the board.
+     *
+     * @return the height of the board
+     */
     public int getHeight() {
 	return boardHeight;
     }
 
+    /**
+     * Returns the width of the board.
+     *
+     * @return the width of the board
+     */
     public int getWidth() {
 	return boardWidth;
     }
 
+    /**
+     * Displays the board using a simple text format.
+     */
     public void display() {
 	for (int j = getHeight() - 1; j >= 0; j--) {
 
@@ -76,7 +140,7 @@ public class BattleBoard {
 
 		switch (getBoardPosition(i, j)) {
 		    case POS_VACANT : b = "   "; break;
-		    case POS_NUKED  : b = " # "; break;
+		    case POS_BOMBED : b = " # "; break;
 		    case POS_CITY   : b = " C "; break;
 		    case POS_UNKN   : b = "   "; break;
 		    case POS_NEAR   : b = " + "; break;
@@ -95,6 +159,18 @@ public class BattleBoard {
 	System.out.println();
     }
 
+    /**
+     * Returns the value at the given (x,y) position on the board.
+     *
+     * @param x the <em>x</em> coordinate
+     *
+     * @param y the <em>y</em> coordinate
+     *
+     * @return the value at the given position in the board
+     *
+     * @throws IllegalArgumentException if either of <em>x</em> or
+     * <em>y</em> is outside the board
+     */
     public int getBoardPosition(int x, int y) {
 	if ((x < 0) || (x >= boardWidth)) {
 	    throw new IllegalArgumentException("illegal x: " + x);
@@ -105,15 +181,32 @@ public class BattleBoard {
 	return board[x][y];
     }
 
-    public int nukeBoardPosition(int x, int y) {
+    /**
+     * Drops a bomb on the given board position (which changes the
+     * current contents of the given board position to {@link
+     * #POS_BOMBED}, and returns the result.
+     *
+     * @param x the <em>x</em> coordinate of the bomb
+     *
+     * @param y the <em>y</em> coordinate of the bomb
+     *
+     * @return {@link #HIT} if the given position contains a city,
+     * {@link #NEAR_MISS} if the given position is adjacent to a city
+     * (and not a city itself), or {@link #MISS} if the position is
+     * does not contain nor is adjacent to a city
+     *
+     * @throws IllegalArgumentException if either of <em>x</em> or
+     * <em>y</em> is outside the board
+     */
+    public int bombBoardPosition(int x, int y) {
+	int rc;
+
 	if ((x < 0) || (x >= boardWidth)) {
 	    throw new IllegalArgumentException("illegal x: " + x);
 	}
 	if ((y < 0) || (y >= boardHeight)) {
 	    throw new IllegalArgumentException("illegal y: " + y);
 	}
-
-	int rc;
 
 	if (isHit(x, y)) {
 	    rc = HIT;
@@ -124,10 +217,31 @@ public class BattleBoard {
 	    rc = MISS;
 	}
 
-	board[x][y] = POS_NUKED;
+	board[x][y] = POS_BOMBED;
 	return rc;
     }
 
+    /**
+     * Updates the given board position with the given state.  <p>
+     *
+     * Does not verify that the given state change is actually legal
+     * in terms of actual game-play.  For example, it is possible to
+     * use this method to change a "near miss" to a "hit" or vice
+     * versa.  "Illegal" state changes are permitted in order to allow
+     * this method to be used by a player to keep track of their
+     * <em>guesses</em> about the contents of the boards of the other
+     * players.
+     *
+     * @param state one of {@link #HIT}, {@link #NEAR_MISS}, or {@link
+     * # MISS}.
+     *
+     * @param x the <em>x</em> coordinate of the bomb
+     *
+     * @param y the <em>y</em> coordinate of the bomb
+     *
+     * @throws IllegalArgumentException if either of <em>x</em> or
+     * <em>y</em> is outside the board
+     */
     public int updateBoardPosition(int x, int y, int state) {
 	if ((x < 0) || (x >= boardWidth)) {
 	    throw new IllegalArgumentException("illegal x: " + x);
@@ -136,23 +250,49 @@ public class BattleBoard {
 	    throw new IllegalArgumentException("illegal y: " + y);
 	}
 
-	// XXX Should make sure that the new state is valid with
-	// respect to the previous state.  For example a NEAR_MISS
-	// can't become a CITY, etc.
-
 	int rc = getBoardPosition(x, y);
 	board[x][y] = state;
 	return rc;
     }
 
+    /**
+     * Indicates whether or not the given board has been "lost".  A
+     * board is lost when it contains no cities.
+     *
+     * @return <code>true</code> if the board contains zero un-bombed
+     * cities, <code>false</code> otherwise
+     */
     public boolean lost() {
 	return (cityCount == 0);
     }
 
+    /**
+     * Indicates whether a given position is a hit (currently occupied
+     * by a city).
+     *
+     * @param x the <em>x</em> coordinate of the bomb
+     *
+     * @param y the <em>y</em> coordinate of the bomb
+     *
+     * @return <code>true</code> if the given position contains a
+     * city, <code>false</code> otherwise
+     */
     public boolean isHit(int x, int y) {
 	return (getBoardPosition(x, y) == POS_CITY);
     }
 
+    /**
+     * Indicates whether a given position is a near miss (not
+     * currently occupied by a city, but adjacent to a position that
+     * is).
+     *
+     * @param x the <em>x</em> coordinate of the bomb
+     *
+     * @param y the <em>y</em> coordinate of the bomb
+     *
+     * @return <code>true</code> if the given position is a near miss,
+     * <code>false</code> otherwise
+     */
     public boolean isNearMiss(int x, int y) {
 
 	// Double-check for off-by-one errors!
@@ -161,6 +301,10 @@ public class BattleBoard {
 	int min_y = (y <= 0) ? y : y - 1;
 	int max_x = (x >= (getWidth() - 1)) ? x : x + 1;
 	int max_y = (y >= (getHeight() - 1)) ? y : y + 1;
+
+	if (isHit(x, y)) {
+	    return false;
+	}
 
 	for (int i = min_x; i <= max_x; i++) {
 	    for (int j = min_y; j <= max_y; j++) {
