6ec72a74da38d58699509b524f3d2489aff00aee
de153050 <de153050@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-02-21 14:48:50 +0000

    New trace record type for the "newName" method. -DJE

---
+++ src/com/sun/gi/objectstore/tso/dataspace/monitor/NewNameTraceRecord.java
@@ -0,0 +1,34 @@
+
+package com.sun.gi.objectstore.tso.dataspace.monitor;
+
+import com.sun.gi.objectstore.tso.dataspace.DataSpace;
+import java.io.Serializable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class NewNameTraceRecord extends TraceRecord implements Serializable {
+    private static final long serialVersionUID = 1L;
+    protected final String name;
+
+    public NewNameTraceRecord(long startTime, String name) {
+	super(startTime);
+	this.name = name;
+    }
+
+    public void replay(DataSpace dataSpace, ReplayState replayState) {
+	// OK.
+	boolean rc = dataSpace.newName(name);
+	boolean expectedRc = replayState.setNewName(name);
+
+	if (rc != expectedRc) {
+	    System.out.println("Contradiction: name = (" + name + ")" +
+		    " expected rc = " + expectedRc + " got = " + rc);
+	}
+    }
+
+    private void readObject(ObjectInputStream in)   
+	    throws IOException, ClassNotFoundException
+    {
+	in.defaultReadObject();
+    }
+}
