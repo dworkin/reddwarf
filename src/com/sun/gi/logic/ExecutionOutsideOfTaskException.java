5e139e9bdeeda7199dae494cf9b9fae54521d594
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-02-24 16:45:08 +0000

    forgot to add this. sorry

---
+++ src/com/sun/gi/logic/ExecutionOutsideOfTaskException.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * <p>Title: ExecutionOutsideOfTaskException.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2004 Sun Microsystems, Inc.</p>
+ * <p>Company: Sun Microsystems, Inc</p>
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+package com.sun.gi.logic;
+
+/**
+ *
+ * <p>Title: ExecutionOutsideOfTaskException.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2004 Sun Microsystems, Inc.</p>
+ * <p>Company: Sun Microsystems, Inc</p>
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+public class ExecutionOutsideOfTaskException extends RuntimeException {
+
+}
