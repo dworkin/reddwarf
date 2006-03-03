4999e2c28bc727cd1ba12fca7d9b45cb8f291653
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-03 03:04:37 +0000

    fixed typo that snuck in with copyright in BattleBoardBoot

--- src/com/sun/gi/apps/jeffboard/BattleBoardBoot.java
+++ src/com/sun/gi/apps/jeffboard/BattleBoardBoot.java
@@ -1,4 +1,4 @@
-/**
+ /**
  *
  * <p>Title: BattleBoardBoot.java</p>
  * 
@@ -37,12 +37,7 @@
      *
      *****************************************************************************/
 
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2004 Sun Microsystems, Inc.</p>
- * <p>Company: Sun Microsystems, Inc</p>
- * @author Jeff Kesselman
- * @version 1.0
- */
+ 
 package com.sun.gi.apps.jeffboard;
 
 import java.security.Principal;
