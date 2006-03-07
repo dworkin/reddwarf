c4315f5e42a28ed637f0eb0dbed4eefecbbc8d13
jmegq <jmegq@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-07 09:05:42 +0000

    Use Generics in StringUtils.  Reformatted.

--- src/com/sun/gi/utils/types/BYTEARRAY.java
+++ src/com/sun/gi/utils/types/BYTEARRAY.java
@@ -1,86 +1,85 @@
 package com.sun.gi.utils.types;
 
 public class BYTEARRAY implements Comparable {
-	byte[] data;
+    byte[] data;
 
-	public BYTEARRAY(byte[] data) {
-		this.data = new byte[data.length];
-		System.arraycopy(data, 0, this.data, 0, data.length);
-	}
+    public BYTEARRAY(byte[] data) {
+        this.data = new byte[data.length];
+        System.arraycopy(data, 0, this.data, 0, data.length);
+    }
 
-	/**
-	 * compareTo
-	 * 
-	 * @param o
-	 *            Object
-	 * @return int
-	 */
-	public int compareTo(Object o) {
-		byte[] otherdata;
-		if (o instanceof byte[]){
-			otherdata = (byte[])o;
-		} else {
-			otherdata = ((BYTEARRAY) o).data;
-		}
-		if (data.length < otherdata.length) {
-			return -1;
-		} else if (data.length > otherdata.length) {
-			return 1;
-		} else {
-			for (int i = 0; i < data.length; i++) {
-				if (data[i] < otherdata[i]) {
-					return -1;
-				} else if (data[i] > otherdata[i]) {
-					return 1;
-				}
-			}
-			return 0;
-		}
-	}
+    /**
+     * compareTo
+     * 
+     * @param o Object
+     * @return int
+     */
+    public int compareTo(Object o) {
+        byte[] otherdata;
+        if (o instanceof byte[]) {
+            otherdata = (byte[]) o;
+        } else {
+            otherdata = ((BYTEARRAY) o).data;
+        }
+        if (data.length < otherdata.length) {
+            return -1;
+        } else if (data.length > otherdata.length) {
+            return 1;
+        } else {
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] < otherdata[i]) {
+                    return -1;
+                } else if (data[i] > otherdata[i]) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
 
-	/**
-	 * data
-	 * 
-	 * @return byte[]
-	 */
-	public byte[] data() {
-		return data;
-	}
+    /**
+     * data
+     * 
+     * @return byte[]
+     */
+    public byte[] data() {
+        return data;
+    }
 
-	/**
-	 * Indicates whether some other object is "equal to" this one.
-	 * 
-	 * @param obj
-	 *            the reference object with which to compare.
-	 * @return <code>true</code> if this object is the same as the obj
-	 *         argument; <code>false</code> otherwise.
-	 * @todo Implement this java.lang.Object method
-	 */
-	public boolean equals(Object obj) {
-		return (compareTo(obj) == 0);
-	}
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * 
+     * @param obj
+     *            the reference object with which to compare.
+     * @return <code>true</code> if this object is the same as the obj
+     *         argument; <code>false</code> otherwise.
+     * @todo Implement this java.lang.Object method
+     */
+    public boolean equals(Object obj) {
+        return (compareTo(obj) == 0);
+    }
 
-	/**
-	 * Returns a hash code value for the object.
-	 * 
-	 * @return a hash code value for this object.
-	 * @todo Implement this java.lang.Object method
-	 */
-	public int hashCode() {
+    /**
+     * Returns a hash code value for the object.
+     * 
+     * @return a hash code value for this object.
+     * @todo Implement this java.lang.Object method
+     */
+    public int hashCode() {
 
-		int hash = 0;
-		int len = data.length;
-		for (int i = 0; i < len; i++) {
-			hash = 31 * hash + data[i];
-		}
-		return hash;
-	}
+        int hash = 0;
+        int len = data.length;
+        for (int i = 0; i < len; i++) {
+            hash = 31 * hash + data[i];
+        }
+        return hash;
+    }
 
-	public String toString() {
-		return ("BYTEARRAY(" + toHex() + ")");
-	}
+    public String toString() {
+        return ("BYTEARRAY(" + toHex() + ")");
+    }
 
-	public String toHex() {
-		return StringUtils.bytesToHex(data);
-	}
+    public String toHex() {
+        return StringUtils.bytesToHex(data);
+    }
 }
--- src/com/sun/gi/utils/types/StringUtils.java
+++ src/com/sun/gi/utils/types/StringUtils.java
@@ -1,76 +1,67 @@
 package com.sun.gi.utils.types;
 
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
- * @author not attributable
- * @version 1.0
- */
+import java.util.ArrayList;
 
-import java.util.*;
+public abstract class StringUtils {
 
-public class StringUtils {
-
-    public StringUtils() { }
-
-    public static String[] explode(String str,String delim) {
-	return explode(str,delim,true);
+    public static String[] explode(String str, String delim) {
+        return explode(str, delim, true);
     }
 
-    public static String[] explode(String str,String delim, boolean trim){
-	List plist = new ArrayList();
-	while (str.length() >0) {
-	    int delimpos = str.indexOf(delim);
-	    String leftString = str.substring(0,delimpos);
-	    if (leftString.length()>0){
-		if (trim) {
-		    leftString = leftString.trim();
-		}
-		plist.add(leftString);
-	    }
-	    str = str.substring(delimpos+delim.length());
-	}
-	String[] retarray= new String[plist.size()];
-	return (String[])plist.toArray(retarray);
+    public static String[] explode(String str, String delim, boolean trim) {
+        ArrayList<String> plist = new ArrayList<String>();
+        while (str.length() > 0) {
+            int delimpos = str.indexOf(delim);
+            String leftString = str.substring(0, delimpos);
+            if (leftString.length() > 0) {
+                if (trim) {
+                    leftString = leftString.trim();
+                }
+                plist.add(leftString);
+            }
+            str = str.substring(delimpos + delim.length());
+        }
+        String[] retarray = new String[plist.size()];
+        return plist.toArray(retarray);
     }
 
-    public static String bytesToHex(byte[] bytes,int start, int length){
-	StringBuffer buff = new StringBuffer();
-	for(int i=0;i<length;i++){
-	    buff.append(byteToHex(bytes[i+start]));
-	}
-	return buff.toString();
+    public static String bytesToHex(byte[] bytes, int start, int length) {
+        StringBuffer buff = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            buff.append(byteToHex(bytes[i + start]));
+        }
+        return buff.toString();
     }
 
-    public static String bytesToHex(byte[] bytes){
-	return bytesToHex(bytes,0,bytes.length);
+    public static String bytesToHex(byte[] bytes) {
+        return bytesToHex(bytes, 0, bytes.length);
     }
 
-    private static String[] hexDigits = {"A","B","C","D","E","F"};
-    public static String byteToHex(byte b){
-	return halfByteToHex((byte)(b>>4))+halfByteToHex((byte)(b&0xF));
+    private static String[] hexDigits = { "A", "B", "C", "D", "E", "F" };
+
+    public static String byteToHex(byte b) {
+        return halfByteToHex((byte) (b >> 4)) + halfByteToHex((byte) (b & 0xF));
     }
 
-    private static String halfByteToHex(byte b){
-	b &= (byte)0xF;
-	if (b<10) return String.valueOf((int)b);
-	return hexDigits[b-10];
+    private static String halfByteToHex(byte b) {
+        b &= (byte) 0xF;
+        if (b < 10)
+            return String.valueOf((int) b);
+        return hexDigits[b - 10];
     }
 
-    public static String bytesToQuads(byte[] bytes){
-	StringBuffer buff = new StringBuffer();
-	for(int i=0;i<bytes.length;i++){
-	    buff.append(String.valueOf((int)(bytes[i])));
-	    if (i+1<bytes.length){
-		buff.append(":");
-	    }
-	}
-	return buff.toString();
+    public static String bytesToQuads(byte[] bytes) {
+        StringBuffer buff = new StringBuffer();
+        for (int i = 0; i < bytes.length; i++) {
+            buff.append(String.valueOf((int) (bytes[i])));
+            if (i + 1 < bytes.length) {
+                buff.append(":");
+            }
+        }
+        return buff.toString();
     }
 
     public static String bytesToHex(byte[] from, int i) {
-	return bytesToHex(from,i,from.length-i);
+        return bytesToHex(from, i, from.length - i);
     }
 }
