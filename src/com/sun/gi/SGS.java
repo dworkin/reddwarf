ecfeba5a343fc5c2907d7b3d8abe7f56cec58eb9
jmegq <jmegq@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-08 00:56:20 +0000

    Add copyright notice, remove old copyrights, reformat, minor style things to appease eclipse.

--- src/com/sun/gi/SGS.java
+++ src/com/sun/gi/SGS.java
@@ -1,83 +1,73 @@
-package com.sun.gi;
 /*
-    Copyright © 2006 Sun Microsystems, Inc., 
-    4150 Network Circle, Santa Clara, California 95054, U.S.A. 
-    All rights reserved.
-
-    Sun Microsystems, Inc. has intellectual property rights relating to 
-    technology embodied in the product that is described in this 
-    document. In particular, and without limitation, these intellectual 
-    property rights may include one or more of the U.S. patents listed 
-    at http://www.sun.com/patents and one or more additional patents or
-    pending patent applications in the U.S. and in other countries.
-
-    U.S. Government Rights - Commercial software. Government users are 
-    subject to the Sun Microsystems, Inc. standard license agreement 
-    and applicable provisions of the FAR and its supplements.
-
-    Use is subject to license terms.
-
-    This distribution may include materials developed by third parties.
-
-    Sun, Sun Microsystems, the Sun logo and Java are trademarks or 
-    registered trademarks of Sun Microsystems, Inc. in the U.S. and 
-    other countries.
-
-    This product is covered and controlled by U.S. Export Control laws 
-    and may be subject to the export or import laws in other countries.
-    Nuclear, missile, chemical biological weapons or nuclear maritime 
-    end uses or end users, whether direct or indirect, are strictly 
-    prohibited. Export or reexport to countries subject to U.S. embargo 
-    or to entities identified on U.S. export exclusion lists, including,
-    but not limited to, the denied persons and specially designated 
-    nationals lists is strictly prohibited.
-
-    Copyright © 2006 Sun Microsystems, Inc., 
-    4150 Network Circle, Santa Clara, California 95054, 
-    Etats-Unis. Tous droits réservés.
-
-    Sun Microsystems, Inc. détient les droits de propriété intellectuels 
-    relatifs à la technologie incorporée dans le produit qui est 
-    décrit dans ce document. En particulier, et ce sans limitation, 
-    ces droits de propriété intellectuelle peuvent inclure un ou plus 
-    des brevets américains listés à l'adresse 
-    http://www.sun.com/patents et un ou les brevets supplémentaires ou 
-    les applications de brevet en attente aux Etats - Unis et dans les 
-    autres pays.
-
-    L'utilisation est soumise aux termes de la Licence.
-
-    Cette distribution peut comprendre des composants développés par des
-    tierces parties.
-
-    Sun, Sun Microsystems, le logo Sun et Java sont des marques de 
-    fabrique ou des marques déposées de Sun Microsystems, Inc. aux 
-    Etats-Unis et dans d'autres pays.
-
-    Ce produit est soumis à la législation américaine en matière de 
-    contrôle des exportations et peut être soumis à la règlementation 
-    en vigueur dans d'autres pays dans le domaine des exportations et 
-    importations. Les utilisations, ou utilisateurs finaux, pour des 
-    armes nucléaires,des missiles, des armes biologiques et chimiques 
-    ou du nucléaire maritime, directement ou indirectement, sont 
-    strictement interdites. Les exportations ou réexportations vers 
-    les pays sous embargo américain, ou vers des entités figurant sur 
-    les listes d'exclusion d'exportation américaines, y compris, mais 
-    de manière non exhaustive, la liste de personnes qui font objet 
-    d'un ordre de ne pas participer, d'une façon directe ou indirecte,
-    aux exportations des produits ou des services qui sont régis par 
-    la législation américaine en matière de contrôle des exportations 
-    et la liste de ressortissants spécifiquement désignés, sont 
-    rigoureusement interdites.
+ * Copyright © 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, California 95054, U.S.A. All rights reserved.
+ * 
+ * Sun Microsystems, Inc. has intellectual property rights relating to
+ * technology embodied in the product that is described in this
+ * document. In particular, and without limitation, these intellectual
+ * property rights may include one or more of the U.S. patents listed at
+ * http://www.sun.com/patents and one or more additional patents or
+ * pending patent applications in the U.S. and in other countries.
+ * 
+ * U.S. Government Rights - Commercial software. Government users are
+ * subject to the Sun Microsystems, Inc. standard license agreement and
+ * applicable provisions of the FAR and its supplements.
+ * 
+ * Use is subject to license terms.
+ * 
+ * This distribution may include materials developed by third parties.
+ * 
+ * Sun, Sun Microsystems, the Sun logo and Java are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other
+ * countries.
+ * 
+ * This product is covered and controlled by U.S. Export Control laws
+ * and may be subject to the export or import laws in other countries.
+ * Nuclear, missile, chemical biological weapons or nuclear maritime end
+ * uses or end users, whether direct or indirect, are strictly
+ * prohibited. Export or reexport to countries subject to U.S. embargo
+ * or to entities identified on U.S. export exclusion lists, including,
+ * but not limited to, the denied persons and specially designated
+ * nationals lists is strictly prohibited.
+ * 
+ * Copyright © 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, California 95054, Etats-Unis. Tous droits réservés.
+ * 
+ * Sun Microsystems, Inc. détient les droits de propriété intellectuels
+ * relatifs à la technologie incorporée dans le produit qui est décrit
+ * dans ce document. En particulier, et ce sans limitation, ces droits
+ * de propriété intellectuelle peuvent inclure un ou plus des brevets
+ * américains listés à l'adresse http://www.sun.com/patents et un ou les
+ * brevets supplémentaires ou les applications de brevet en attente aux
+ * Etats - Unis et dans les autres pays.
+ * 
+ * L'utilisation est soumise aux termes de la Licence.
+ * 
+ * Cette distribution peut comprendre des composants développés par des
+ * tierces parties.
+ * 
+ * Sun, Sun Microsystems, le logo Sun et Java sont des marques de
+ * fabrique ou des marques déposées de Sun Microsystems, Inc. aux
+ * Etats-Unis et dans d'autres pays.
+ * 
+ * Ce produit est soumis à la législation américaine en matière de
+ * contrôle des exportations et peut être soumis à la règlementation en
+ * vigueur dans d'autres pays dans le domaine des exportations et
+ * importations. Les utilisations, ou utilisateurs finaux, pour des
+ * armes nucléaires,des missiles, des armes biologiques et chimiques ou
+ * du nucléaire maritime, directement ou indirectement, sont strictement
+ * interdites. Les exportations ou réexportations vers les pays sous
+ * embargo américain, ou vers des entités figurant sur les listes
+ * d'exclusion d'exportation américaines, y compris, mais de manière non
+ * exhaustive, la liste de personnes qui font objet d'un ordre de ne pas
+ * participer, d'une façon directe ou indirecte, aux exportations des
+ * produits ou des services qui sont régis par la législation américaine
+ * en matière de contrôle des exportations et la liste de ressortissants
+ * spécifiquement désignés, sont rigoureusement interdites.
  */
 
-/**
- *
- * <p>Title: SGS </p>
- * <p>Description: The Sun Game Server.  This is the master app class that gets
- * instantiated.  It coordinbates the creation and inter-wiring of all the
- * levels of the game sever that are resident in a  single slice.</p>
- */
+package com.sun.gi;
+
 import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.util.Iterator;
@@ -115,25 +105,27 @@ import com.sun.gi.objectstore.tso.dataspace.PersistantInMemoryDataSpace;
 import com.sun.gi.utils.SGSUUID;
 import com.sun.gi.utils.StatisticalUUID;
 
+/**
+ * 
+ * <p>
+ * Title: SGS
+ * </p>
+ * <p>
+ * Description: The Sun Game Server. This is the master app class that
+ * gets instantiated. It coordinbates the creation and inter-wiring of
+ * all the levels of the game sever that are resident in a single slice.
+ * </p>
+ */
 public class SGS {
     ReportManager reportManager;
-
     TimerManager timerManager;
-
     TransportManager transportManager;
-
     ReportUpdater reportUpdater;
-
     SGSUUID sliceID = new StatisticalUUID();
-
     SimKernel kernel;
-
     ObjectStore ostore;
-
     private boolean verbose = false;
-
     private String installFile = "file:Install.txt";
-
     private static final long REPORTTTL = 1000;
 
     public SGS() {
@@ -143,8 +135,7 @@ public class SGS {
 	}
 	try {
 	    kernel = new SimKernelImpl();
-	    String installProperty = System
-		.getProperty("sgs.framework.installurl");
+	    String installProperty = System.getProperty("sgs.framework.installurl");
 	    if (installProperty != null) {
 		installFile = installProperty;
 	    }
@@ -152,7 +143,7 @@ public class SGS {
 		System.out.println("Loading configuration from: " + installFile);
 	    }
 	    InstallationLoader installation = new InstallationURL(new URL(
-			installFile));
+		    installFile));
 	    // start framework services
 	    transportManager = new LRMPTransportManager();
 	    reportManager = new ReportManagerImpl(transportManager, REPORTTTL);
@@ -167,20 +158,20 @@ public class SGS {
 	    kernel.setRawSocketManager(new RawSocketManagerImpl());
 
 	    // start game services
-	    StatusReport installationReport = reportManager
-		.makeNewReport("_SGS_discover_" + sliceID);
+	    StatusReport installationReport =
+                reportManager.makeNewReport("_SGS_discover_" + sliceID);
 
 	    TransportManager routerTransportManager;
 	    /*
-	     * XXX: For single-stack case, use a non-multicast, no-op
-	     *      transport at the router layer.  This is both faster
-	     *      and works around the lack of message-fragmentation
-	     *      in LRMP (which limits message sizes to UDP-max)
-	     *
-	     *      To use the LRMP routing, use:
-	     *        routerTransportManager = transportManager;
-	     *      -jm
-	     */
+                 * XXX: For single-stack case, use a non-multicast,
+                 * no-op transport at the router layer. This is both
+                 * faster and works around the lack of
+                 * message-fragmentation in LRMP (which limits message
+                 * sizes to UDP-max)
+                 * 
+                 * To use the LRMP routing, use: routerTransportManager =
+                 * transportManager; -jm
+                 */
 	    routerTransportManager = new NullTransportManager();
 
 	    installationReport.setParameter("game", "count", "0");
@@ -198,11 +189,10 @@ public class SGS {
     }
 
     /**
-     * startupGame
-     * 
-     * @param game
-     *            InstallRec
-     */
+         * startupGame
+         * 
+         * @param game InstallRec
+         */
     private Router startupGame(TransportManager transportMgr,
 	    DeploymentRec game, StatusReport installationReport) {
 	Router router = null;
@@ -215,14 +205,13 @@ public class SGS {
 	}
 
 	int gameCount = Integer.parseInt(installationReport.getParameter(
-		    "game", "count"));
+		"game", "count"));
 	String statusBlockName = "game." + gameCount;
-	installationReport.setParameter(statusBlockName, "id", Integer
-		.toString(gameID));
-	installationReport.setParameter(statusBlockName, "description", game
-		.getDescription());
-	installationReport
-	    .setParameter(statusBlockName, "name", game.getName());
+	installationReport.setParameter(statusBlockName, "id",
+		Integer.toString(gameID));
+	installationReport.setParameter(statusBlockName, "description",
+		game.getDescription());
+	installationReport.setParameter(statusBlockName, "name", game.getName());
 	int umgrCount = 0;
 
 	// create simulation container for game
@@ -230,9 +219,8 @@ public class SGS {
 	    Simulation sim = null;
 	    try {
 		ostore = new TSOObjectStore(new PersistantInMemoryDataSpace(
-			    gameID));
-		String cleanProperty = System
-		    .getProperty("sgs.ostore.startclean");
+			gameID));
+		String cleanProperty = System.getProperty("sgs.ostore.startclean");
 		if ((cleanProperty != null)
 			&& (cleanProperty.equalsIgnoreCase("true"))) {
 		    if (verbose) {
@@ -257,11 +245,10 @@ public class SGS {
 		    Integer.toString(umgrCount));
 	    try {
 		Class serverClass = Class.forName(serverClassName);
-		Constructor constructor = serverClass
-		    .getConstructor(new Class[] { Router.class, Map.class });
-		UserManager umgr = (UserManager) constructor
-		    .newInstance(new Object[] { router,
-			    umgrRec.getParameterMap() });
+		Constructor constructor = serverClass.getConstructor(new Class[] {
+			Router.class, Map.class });
+		UserManager umgr = (UserManager) constructor.newInstance(new Object[] {
+			router, umgrRec.getParameterMap() });
 		installationReport.setParameter(umgrBlock, "clientClassName",
 			umgr.getClientClassname());
 		Set clientParams = umgr.getClientParams().entrySet();
@@ -279,12 +266,9 @@ public class SGS {
 		}
 		if (umgrRec.hasValidatorModules()) {
 		    UserValidatorFactory validatorFactory = new UserValidatorFactoryImpl();
-		    for (ValidatorRec lmoduleRec : umgrRec
-			    .getValidatorModules()) {
-			String loginModuleClassname = lmoduleRec
-			    .getValidatorClassName();
-			Class loginModuleClass = Class
-			    .forName(loginModuleClassname);
+		    for (ValidatorRec lmoduleRec : umgrRec.getValidatorModules()) {
+			String loginModuleClassname = lmoduleRec.getValidatorClassName();
+			Class loginModuleClass = Class.forName(loginModuleClassname);
 			validatorFactory.addLoginModule(loginModuleClass,
 				lmoduleRec.getParameterMap());
 		    }
@@ -294,14 +278,15 @@ public class SGS {
 
 		// add client to list
 
-		// need to start boot method in container if it has one here.
+		// need to start boot method in container if it has one
+		// here.
 
 	    } catch (Exception ex) {
 		ex.printStackTrace();
 	    }
 	    gameCount++;
-	    installationReport.setParameter("game", "count", Integer
-		    .toString(gameCount));
+	    installationReport.setParameter("game", "count",
+		    Integer.toString(gameCount));
 	}
 
 	return router;
@@ -311,20 +296,22 @@ public class SGS {
 	for (int i = 0; i < args.length; i++) {
 	    if (args[i].charAt(0) == '-') {
 		switch (args[i].charAt(1)) {
-		case 'v':
-		case 'V':
-		    System.setProperty("sgs.framework.verbose", "true");
-		    break;
-		case 'i':
-		case 'I':
-		    System.setProperty("sgs.framework.installurl", args[++i]);
-		    break;
-		case 'c':
-		case 'C':
-		    System.setProperty("sgs.ostore.startclean", "true");
-		    break;
-		default:
-		    System.err.println("Unrecognized switch `" + args[i] + "'");
+		    case 'v':
+		    case 'V':
+			System.setProperty("sgs.framework.verbose", "true");
+			break;
+		    case 'i':
+		    case 'I':
+			System.setProperty("sgs.framework.installurl",
+				args[++i]);
+			break;
+		    case 'c':
+		    case 'C':
+			System.setProperty("sgs.ostore.startclean", "true");
+			break;
+		    default:
+			System.err.println("Unrecognized switch `" + args[i]
+				+ "'");
 		}
 	    } else {
 		System.err.println("Unrecognized argument `" + args[i] + "'");
