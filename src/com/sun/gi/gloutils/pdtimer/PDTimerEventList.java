b4790fca9171f19d414e1f86bce9dbc6ed7ccd98
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-08 06:38:45 +0000

    introduced a simple bucket scheme to handle time collisions in the map

--- src/com/sun/gi/gloutils/pdtimer/PDTimerEventList.java
+++ src/com/sun/gi/gloutils/pdtimer/PDTimerEventList.java
@@ -2,6 +2,7 @@ package com.sun.gi.gloutils.pdtimer;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.SortedMap;
@@ -34,7 +35,8 @@ import com.sun.gi.logic.SimTask.ACCESS_TYPE;
  */
 public class PDTimerEventList implements GLO {
         private static Logger log = Logger.getLogger("com.sun.gi.gloutils.pdtimer");
-	private SortedMap<Long, GLOReference> timerEvents = new TreeMap<Long, GLOReference>();
+	private SortedMap<Long, HashSet<GLOReference>> timerEvents =
+        new TreeMap<Long, HashSet<GLOReference>>();
 	private static final int BIGCLEANUP_PERIOD = 100;
 	private int bigCleanupCountdown = BIGCLEANUP_PERIOD;
 	
@@ -54,18 +56,19 @@ public class PDTimerEventList implements GLO {
 		task.access_check(ACCESS_TYPE.PEEK,this);
 		log.finest("Ticking timer list");
 		List<GLOReference> cleanupList = new ArrayList<GLOReference>();
-		for (Entry<Long, GLOReference> entry : timerEvents.entrySet()) {
+		for (Entry<Long, HashSet<GLOReference>> entry : timerEvents.entrySet()) {
 			if (entry.getKey() <= time) {
-				GLOReference eventRef = entry.getValue();
-				PDTimerEvent event = (PDTimerEvent) eventRef.attempt(task);
-				if (event != null) { // if null then we can skip
-					if (event.isActive()) { // if active then do it
-						event.fire(task);
-						if (event.requiresCleanup()) {
-							cleanupList.add(eventRef);
-						}
-					}
-				}
+                for (GLOReference eventRef : entry.getValue()) {
+                    PDTimerEvent event = (PDTimerEvent) eventRef.attempt(task);
+                    if (event != null) { // if null then we can skip
+                        if (event.isActive()) { // if active then do it
+                            event.fire(task);
+                            if (event.requiresCleanup()) {
+                                cleanupList.add(eventRef);
+                            }
+                        }
+                    }
+                }
 			} else {
 				break; // out of events
 			}
@@ -97,9 +100,15 @@ public class PDTimerEventList implements GLO {
 	public void addEvent(SimTask task, GLOReference evntRef) {
 		task.access_check(ACCESS_TYPE.GET,this);
 		PDTimerEvent evnt = (PDTimerEvent) evntRef.peek(task);
-		timerEvents.put(new Long(evnt.delayTime() + System.currentTimeMillis()),
-				evntRef);
+        long fireTime = evnt.delayTime() + System.currentTimeMillis();
+        HashSet bucket = timerEvents.get(fireTime);
 
+        if (bucket == null) {
+            bucket = new HashSet();
+            timerEvents.put(new Long(fireTime), bucket);
+        }
+
+        bucket.add(evntRef);
 	}
 
 	/**
@@ -107,13 +116,17 @@ public class PDTimerEventList implements GLO {
 	 * @param eventRef
 	 */
 	public void removeEvent(GLOReference eventRef) {
-		for (Iterator i = timerEvents.entrySet().iterator(); i.hasNext();) {
-			Entry entry = (Entry) i.next();
-			if (eventRef.equals(entry.getValue())) {
-				i.remove();
-				return;
-			}
-		}
+        for (Iterator it = timerEvents.entrySet().iterator(); it.hasNext();) {
+            Entry<Long,HashSet<GLOReference>> entry =
+                (Entry<Long,HashSet<GLOReference>>) it.next();
+            if (entry.getValue().contains(eventRef)) {
+                if (entry.getValue().size() == 1)
+                    it.remove();
+                else
+                    entry.getValue().remove(eventRef);
+                return;
+            }
+        }
 	}
 
 	// called from a task
@@ -122,19 +135,19 @@ public class PDTimerEventList implements GLO {
 		SimTask task = SimTask.getCurrent();
 		task.access_check(ACCESS_TYPE.GET,this);
 		log.finest("Doing cleanup");
-		if (--bigCleanupCountdown==0){ // do a big cleanup
+		/*if (--bigCleanupCountdown==0){ // do a big cleanup
 			bigCleanup(task);
-		} else { // do a normal destributed cleanup
+            } else*/ { // do a normal destributed cleanup
 			for (GLOReference ref : cleanupList) {
 				PDTimerEvent evnt = (PDTimerEvent) ref.get(task);
 				if (evnt.isRepeating()) {					
 					removeEvent(ref);
 					evnt.reset(task); // resets it for next ring
 					addEvent(task, ref);
-				} else if (evnt.isMoribund()) {
+                    } else if (evnt.isMoribund()) {
 					removeEvent(ref);
 					ref.delete(task);
-				}
+                    }
 			}
 		}
 	}
@@ -143,20 +156,22 @@ public class PDTimerEventList implements GLO {
 	// * (DESIGNED To BE CALLED WITH ACCESS.GET)
 	private void bigCleanup(SimTask task){
 		long time = System.currentTimeMillis();
-		for(Entry<Long,GLOReference> entry : timerEvents.entrySet()){
+		for(Entry<Long,HashSet<GLOReference>> entry : timerEvents.entrySet()){
 			if(entry.getKey().longValue()<=time){
-				GLOReference ref = entry.getValue();
-				PDTimerEvent evnt = (PDTimerEvent)ref.get(task);
-				if (evnt.requiresCleanup()){ //needs to be cleaned					
-					if (evnt.isRepeating()) {					
-						removeEvent(ref);
-						evnt.reset(task); // resets it for next ring
-						addEvent(task, ref);
-					} else if (evnt.isMoribund()) {
-						removeEvent(ref);
-						ref.delete(task);
-					}
-				}
+                for (GLOReference ref : entry.getValue()) {
+                    PDTimerEvent evnt = (PDTimerEvent)ref.get(task);
+                    if (evnt.requiresCleanup()){ //needs to be cleaned					
+                        if (evnt.isRepeating()) {					
+                            System.out.println("re-installing");
+                            removeEvent(ref);
+                            evnt.reset(task); // resets it for next ring
+                            addEvent(task, ref);
+                        } else if (evnt.isMoribund()) {
+                            removeEvent(ref);
+                            ref.delete(task);
+                        }
+                    }
+                }
 			} else {
 				break; // end of events that have fired already
 			}
