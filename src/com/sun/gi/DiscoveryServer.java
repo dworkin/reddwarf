172e5b1f95e4b0094f36ef81363e413febd18945
jmegq <jmegq@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-08 01:04:54 +0000

    Add copyright notice, remove old copyrights, reformat, minor style things to appease eclipse.

--- src/com/sun/gi/DiscoveryServer.java
+++ src/com/sun/gi/DiscoveryServer.java
@@ -1,19 +1,89 @@
+/*
+ * Copyright © 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, California 95054, U.S.A. All rights reserved.
+ * 
+ * Sun Microsystems, Inc. has intellectual property rights relating to
+ * technology embodied in the product that is described in this
+ * document. In particular, and without limitation, these intellectual
+ * property rights may include one or more of the U.S. patents listed at
+ * http://www.sun.com/patents and one or more additional patents or
+ * pending patent applications in the U.S. and in other countries.
+ * 
+ * U.S. Government Rights - Commercial software. Government users are
+ * subject to the Sun Microsystems, Inc. standard license agreement and
+ * applicable provisions of the FAR and its supplements.
+ * 
+ * Use is subject to license terms.
+ * 
+ * This distribution may include materials developed by third parties.
+ * 
+ * Sun, Sun Microsystems, the Sun logo and Java are trademarks or
+ * registered trademarks of Sun Microsystems, Inc. in the U.S. and other
+ * countries.
+ * 
+ * This product is covered and controlled by U.S. Export Control laws
+ * and may be subject to the export or import laws in other countries.
+ * Nuclear, missile, chemical biological weapons or nuclear maritime end
+ * uses or end users, whether direct or indirect, are strictly
+ * prohibited. Export or reexport to countries subject to U.S. embargo
+ * or to entities identified on U.S. export exclusion lists, including,
+ * but not limited to, the denied persons and specially designated
+ * nationals lists is strictly prohibited.
+ * 
+ * Copyright © 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, California 95054, Etats-Unis. Tous droits réservés.
+ * 
+ * Sun Microsystems, Inc. détient les droits de propriété intellectuels
+ * relatifs à la technologie incorporée dans le produit qui est décrit
+ * dans ce document. En particulier, et ce sans limitation, ces droits
+ * de propriété intellectuelle peuvent inclure un ou plus des brevets
+ * américains listés à l'adresse http://www.sun.com/patents et un ou les
+ * brevets supplémentaires ou les applications de brevet en attente aux
+ * Etats - Unis et dans les autres pays.
+ * 
+ * L'utilisation est soumise aux termes de la Licence.
+ * 
+ * Cette distribution peut comprendre des composants développés par des
+ * tierces parties.
+ * 
+ * Sun, Sun Microsystems, le logo Sun et Java sont des marques de
+ * fabrique ou des marques déposées de Sun Microsystems, Inc. aux
+ * Etats-Unis et dans d'autres pays.
+ * 
+ * Ce produit est soumis à la législation américaine en matière de
+ * contrôle des exportations et peut être soumis à la règlementation en
+ * vigueur dans d'autres pays dans le domaine des exportations et
+ * importations. Les utilisations, ou utilisateurs finaux, pour des
+ * armes nucléaires,des missiles, des armes biologiques et chimiques ou
+ * du nucléaire maritime, directement ou indirectement, sont strictement
+ * interdites. Les exportations ou réexportations vers les pays sous
+ * embargo américain, ou vers des entités figurant sur les listes
+ * d'exclusion d'exportation américaines, y compris, mais de manière non
+ * exhaustive, la liste de personnes qui font objet d'un ordre de ne pas
+ * participer, d'une façon directe ou indirecte, aux exportations des
+ * produits ou des services qui sont régis par la législation américaine
+ * en matière de contrôle des exportations et la liste de ressortissants
+ * spécifiquement désignés, sont rigoureusement interdites.
+ */
+
 package com.sun.gi;
 
-import java.io.*;
-
-import com.sun.gi.framework.interconnect.*;
-import com.sun.gi.framework.interconnect.impl.*;
-import com.sun.gi.framework.status.*;
-import com.sun.gi.framework.status.impl.*;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+
+import com.sun.gi.framework.interconnect.TransportManager;
+import com.sun.gi.framework.interconnect.impl.LRMPTransportManager;
+import com.sun.gi.framework.status.ReportManager;
+import com.sun.gi.framework.status.StatusReport;
+import com.sun.gi.framework.status.impl.ReportManagerImpl;
 
 public class DiscoveryServer {
 
@@ -59,22 +129,19 @@ public class DiscoveryServer {
     Map<Integer, GameRecord> gameMap;
 
     ReportManager reportManager;
-
     TransportManager transportManager;
 
     static long fileUpdatePeriod = 50000; // regenreate once a second
-
     final static String prefix = "_SGS_discover_";
 
     FileChannel chan;
-
     FileLock lock;
 
     String discoveryFileName;
 
     static {
-        String updatePeriodStr = System
-                .getProperty("sgs.discovery.updateperiod");
+        String updatePeriodStr =
+            System.getProperty("sgs.discovery.updateperiod");
         if (updatePeriodStr != null) {
             fileUpdatePeriod = Integer.parseInt(updatePeriodStr);
         }
@@ -91,8 +158,8 @@ public class DiscoveryServer {
                 startDiscoveryFile("discovery.xml");
                 for (int i = 0; i < reportNames.length; i++) {
                     if (reportNames[i].startsWith(prefix)) {
-                        StatusReport report = reportManager
-                                .getReport(reportNames[i]);
+                        StatusReport report =
+                            reportManager.getReport(reportNames[i]);
                         if (report != null) {
                             report.dump(System.out);
                             addReportToDiscoveryFile(report);
@@ -129,16 +196,14 @@ public class DiscoveryServer {
         buff.put("<DISCOVERY>\n".getBytes());
 
         for (GameRecord grec : gameMap.values()) {
-            buff.put(("\t<GAME id=\"" + grec.id +
-                    "\" name=\"" + grec.name + "\" >\n").getBytes());
+            buff.put(("\t<GAME id=\"" + grec.id + "\" name=\"" + grec.name + "\" >\n").getBytes());
             for (UserManagerRecord urec : grec.userManagerRecords) {
                 buff.put(("\t\t<USERMANAGER clientclass=\""
                         + urec.clientClassName + "\" >\n").getBytes());
 
                 for (Entry<String, String> entry : urec.params.entrySet()) {
                     buff.put(("\t\t\t<PARAMETER tag=\"" + entry.getKey()
-                            + "\" value=\"" + entry.getValue() + "\" />\n")
-                            .getBytes());
+                            + "\" value=\"" + entry.getValue() + "\" />\n").getBytes());
                 }
                 buff.put("\t\t</USERMANAGER>\n".getBytes());
 
@@ -159,7 +224,8 @@ public class DiscoveryServer {
                 fos.close();
                 return;
             } catch (Exception ex) {
-                // temporarily since we cant stop the lock exception, just
+                // temporarily since we cant stop the lock exception,
+                // just
                 // handle it
                 // ex.printStackTrace();
                 try {
@@ -175,8 +241,7 @@ public class DiscoveryServer {
     /**
      * addReportToDiscoveryFile
      * 
-     * @param statusReport
-     *            StatusReport
+     * @param statusReport StatusReport
      */
     private void addReportToDiscoveryFile(StatusReport statusReport) {
         int gameCount = Integer.parseInt(statusReport.getParameter("game",
