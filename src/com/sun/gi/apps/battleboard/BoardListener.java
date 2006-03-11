e573258831e1fe85e9512392943f19fb0c32aa94
de153050 <de153050@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-03-11 02:22:59 +0000

    cosmetic. DJE

--- src/com/sun/gi/apps/battleboard/BoardListener.java
+++ src/com/sun/gi/apps/battleboard/BoardListener.java
@@ -69,18 +69,20 @@
 package com.sun.gi.apps.battleboard;
 
 /**
- * BoardListeners are notified whenever a square on a
- * BattleBoard changes.
+ * BoardListeners are notified whenever a square on a {@link
+ * BattleBoard} changes.
  */
 public interface BoardListener {
 
     /**
-     * Notifies the listener that a particular position on a
-     * <code>BattleBoard</code> has changed.
+     * Notifies the listener that a particular position on a {@link
+     * BattleBoard} has changed.
      *
      * @param board the BattleBoard referenced
+     *
      * @param x the x location of the changed square
+     *
      * @param y the y location of the changed square
      */
-    public void boardChanged(BattleBoard board, int x, int y);
+    void boardChanged(BattleBoard board, int x, int y);
 }
