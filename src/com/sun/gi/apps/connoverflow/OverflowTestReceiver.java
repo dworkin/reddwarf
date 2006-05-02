d61082edaf5a0e779a9e423b7d99999f4fee6061
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-05-02 00:25:24 +0000

    Added connection overflow test, in support of #39

---
+++ src/com/sun/gi/apps/connoverflow/OverflowTestReceiver.java
@@ -0,0 +1,249 @@
+/**
+ * 
+ * <p>
+ * Title: OverflowTestReceiver.java
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2004 Sun Microsystems, Inc.
+ * </p>
+ * <p>
+ * Company: Sun Microsystems, Inc
+ * </p>
+ * 
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+package com.sun.gi.apps.connoverflow;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+
+import javax.security.auth.callback.Callback;
+import javax.swing.JFrame;
+
+import com.sun.gi.apps.swordworld.client.ValidatorDialog;
+import com.sun.gi.comm.discovery.impl.URLDiscoverer;
+import com.sun.gi.comm.users.client.ClientAlreadyConnectedException;
+import com.sun.gi.comm.users.client.ClientChannel;
+import com.sun.gi.comm.users.client.ClientChannelListener;
+import com.sun.gi.comm.users.client.ClientConnectionManager;
+import com.sun.gi.comm.users.client.ClientConnectionManagerListener;
+import com.sun.gi.comm.users.client.impl.ClientConnectionManagerImpl;
+
+/**
+ * 
+ * <p>
+ * Title: OverflowTestReceiver.java
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2004 Sun Microsystems, Inc.
+ * </p>
+ * <p>
+ * Company: Sun Microsystems, Inc
+ * </p>
+ * 
+ * @author Jeff Kesselman
+ * @version 1.0
+ */
+public class OverflowTestReceiver extends JFrame
+        implements ClientConnectionManagerListener, ClientChannelListener
+
+{
+    ClientConnectionManager mgr;
+
+    public OverflowTestReceiver() {
+        setSize(100,100);
+        setVisible(true);
+        try {
+            connect("file:FakeDiscovery.xml");
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+        } catch (ClientAlreadyConnectedException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public void connect(String discoveryURL) throws MalformedURLException,
+            ClientAlreadyConnectedException {
+        mgr = new ClientConnectionManagerImpl("OFLOW", new URLDiscoverer(
+                new URL(discoveryURL)));
+        mgr.setListener(this);
+        mgr.connect("com.sun.gi.comm.users.client.impl.TCPIPUserManagerClient");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new OverflowTestReceiver();
+        while (true)
+            ;
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#validationRequest(javax.security.auth.callback.Callback[])
+     */
+    public void validationRequest(Callback[] callbacks) {
+        ValidatorDialog dialog = new ValidatorDialog(this,callbacks);
+        mgr.sendValidationResponse(callbacks);          
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#connected(byte[])
+     */
+    public void connected(byte[] myID) {
+        // should start crap coming to us
+        // we ARENT going to read it ebcause we WANT a buffer overflow
+        // condition on server
+        System.out.println("receiver connected");
+        mgr.openChannel("junk");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#connectionRefused(java.lang.String)
+     */
+    public void connectionRefused(String message) {
+        System.out.println("Refused: "+message); 
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#failOverInProgress()
+     */
+    public void failOverInProgress() {
+        System.out.println("failing over");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#reconnected()
+     */
+    public void reconnected() {
+        System.out.println("Reconnected");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#disconnected()
+     */
+    public void disconnected() {
+        System.out.println("Disconnected");
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#userJoined(byte[])
+     */
+    public void userJoined(byte[] userID) {
+    // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#userLeft(byte[])
+     */
+    public void userLeft(byte[] userID) {
+    // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#joinedChannel(com.sun.gi.comm.users.client.ClientChannel)
+     */
+    public void joinedChannel(ClientChannel channel) {
+        System.out.println("Joiend channel");
+        channel.setListener(this);
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientConnectionManagerListener#channelLocked(java.lang.String,
+     * byte[])
+     */
+    public void channelLocked(String channelName, byte[] userID) {
+    // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientChannelListener#playerJoined(byte[])
+     */
+    public void playerJoined(byte[] playerID) {
+    // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientChannelListener#playerLeft(byte[])
+     */
+    public void playerLeft(byte[] playerID) {
+    // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientChannelListener#dataArrived(byte[],
+     * java.nio.ByteBuffer, boolean)
+     */
+    public void dataArrived(byte[] from, ByteBuffer data, boolean reliable) {
+        System.out.println("received buffer sized " + data.remaining());
+        //now block it up so we can create an overflow
+        while(true){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sun.gi.comm.users.client.ClientChannelListener#channelClosed()
+     */
+    public void channelClosed() {
+        System.out.println("channel closed");
+
+    }
+
+}
---
+++ src/com/sun/gi/apps/connoverflow/ValidatorDialog.java
@@ -0,0 +1,250 @@
+/*
+ Copyright (c) 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa
+ Clara, California 95054, U.S.A. All rights reserved.
+ 
+ Sun Microsystems, Inc. has intellectual property rights relating to
+ technology embodied in the product that is described in this document.
+ In particular, and without limitation, these intellectual property rights
+ may include one or more of the U.S. patents listed at
+ http://www.sun.com/patents and one or more additional patents or pending
+ patent applications in the U.S. and in other countries.
+ 
+ U.S. Government Rights - Commercial software. Government users are subject
+ to the Sun Microsystems, Inc. standard license agreement and applicable
+ provisions of the FAR and its supplements.
+ 
+ This distribution may include materials developed by third parties.
+ 
+ Sun, Sun Microsystems, the Sun logo and Java are trademarks or registered
+ trademarks of Sun Microsystems, Inc. in the U.S. and other countries.
+ 
+ UNIX is a registered trademark in the U.S. and other countries, exclusively
+ licensed through X/Open Company, Ltd.
+ 
+ Products covered by and information contained in this service manual are
+ controlled by U.S. Export Control laws and may be subject to the export
+ or import laws in other countries. Nuclear, missile, chemical biological
+ weapons or nuclear maritime end uses or end users, whether direct or
+ indirect, are strictly prohibited. Export or reexport to countries subject
+ to U.S. embargo or to entities identified on U.S. export exclusion lists,
+ including, but not limited to, the denied persons and specially designated
+ nationals lists is strictly prohibited.
+ 
+ DOCUMENTATION IS PROVIDED "AS IS" AND ALL EXPRESS OR IMPLIED CONDITIONS,
+ REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ ARE DISCLAIMED, EXCEPT TO THE EXTENT THAT SUCH DISCLAIMERS ARE HELD TO BE
+ LEGALLY INVALID.
+ 
+ Copyright © 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ California 95054, Etats-Unis. Tous droits réservés.
+ 
+ Sun Microsystems, Inc. détient les droits de propriété intellectuels
+ relatifs à la technologie incorporée dans le produit qui est décrit dans
+ ce document. En particulier, et ce sans limitation, ces droits de
+ propriété intellectuelle peuvent inclure un ou plus des brevets américains
+ listés à l'adresse http://www.sun.com/patents et un ou les brevets
+ supplémentaires ou les applications de brevet en attente aux Etats -
+ Unis et dans les autres pays.
+ 
+ Cette distribution peut comprendre des composants développés par des
+ tierces parties.
+ 
+ Sun, Sun Microsystems, le logo Sun et Java sont des marques de fabrique
+ ou des marques déposées de Sun Microsystems, Inc. aux Etats-Unis et dans
+ d'autres pays.
+ 
+ UNIX est une marque déposée aux Etats-Unis et dans d'autres pays et
+ licenciée exlusivement par X/Open Company, Ltd.
+ 
+ see above Les produits qui font l'objet de ce manuel d'entretien et les
+ informations qu'il contient sont regis par la legislation americaine en
+ matiere de controle des exportations et peuvent etre soumis au droit
+ d'autres pays dans le domaine des exportations et importations.
+ Les utilisations finales, ou utilisateurs finaux, pour des armes
+ nucleaires, des missiles, des armes biologiques et chimiques ou du
+ nucleaire maritime, directement ou indirectement, sont strictement
+ interdites. Les exportations ou reexportations vers des pays sous embargo
+ des Etats-Unis, ou vers des entites figurant sur les listes d'exclusion
+ d'exportation americaines, y compris, mais de maniere non exclusive, la
+ liste de personnes qui font objet d'un ordre de ne pas participer, d'une
+ facon directe ou indirecte, aux exportations des produits ou des services
+ qui sont regi par la legislation americaine en matiere de controle des
+ exportations et la liste de ressortissants specifiquement designes, sont
+ rigoureusement interdites.
+ 
+ LA DOCUMENTATION EST FOURNIE "EN L'ETAT" ET TOUTES AUTRES CONDITIONS,
+ DECLARATIONS ET GARANTIES EXPRESSES OU TACITES SONT FORMELLEMENT EXCLUES,
+ DANS LA MESURE AUTORISEE PAR LA LOI APPLICABLE, Y COMPRIS NOTAMMENT TOUTE
+ GARANTIE IMPLICITE RELATIVE A LA QUALITE MARCHANDE, A L'APTITUDE A UNE
+ UTILISATION PARTICULIERE OU A L'ABSENCE DE CONTREFACON.
+*/
+
+package com.sun.gi.apps.connoverflow;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.ChoiceCallback;
+import javax.security.auth.callback.ConfirmationCallback;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.TextInputCallback;
+import javax.security.auth.callback.TextOutputCallback;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ * A Swing GUI for server validation fulfillment.  <p>
+ *
+ * When connecting to a server application via a UserManager, the
+ * UserManager will attempt to validate the user based on the
+ * applications validation settings as specified in its deployment
+ * descriptor.  In the case of the SwordWorld application, a name and
+ * a password are required.
+ * 
+ */
+public class ValidatorDialog extends JDialog {
+
+    private static final long serialVersionUID = 1L;
+
+    // The array of javax.security.auth.callbacks.Callbacks
+    Callback[] callbacks;
+
+    // an array of UI components that parallel callbacks[] in size and order.
+    List<Component> dataFields = new ArrayList<Component>();
+
+    /**
+     * Creates a new ValidatorDialog for the given frame and
+     * callbacks.  <p>
+     *
+     * The dialog iterates through the Callback array and displays the
+     * appropriate UI based on the type of Callback.  In the case of
+     * CommTest, the Callbacks are of type NameCallback and
+     * PasswordCallback.  This causes both a "username" textfield, and
+     * a "password" input field to be rendered on the dialog.
+     * 
+     * @param parent the dialog's parent frame
+     *
+     * @param cbs an array of Callbacks
+     */
+    public ValidatorDialog(Frame parent, Callback[] cbs) {
+        super(parent, "Validation Information Required", true);
+        callbacks = cbs;
+        Container c = getContentPane();
+        JPanel validationPanel = new JPanel();
+        validationPanel.setLayout(new GridLayout(2, 0));
+        c.add(validationPanel, BorderLayout.NORTH);
+        JButton validateButton = new JButton("CONTINUE");
+        c.add(validateButton, BorderLayout.SOUTH);
+
+	/*
+	 * when pressed, set the data from the UI components to the
+	 * matching Callbacks.
+	 */
+        validateButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                transcribeToCallbacks();
+                ValidatorDialog.this.setVisible(false);
+                ValidatorDialog.this.getParent().remove(ValidatorDialog.this);
+            }
+        });
+
+	/*
+	 * Iterate through the javax.security.auth.callback.Callbacks
+	 * and render the appropriate UI accordingly.  For each
+	 * Callback, the matching UI Component is stored in the
+	 * dataFields array.  The order is important, as they will be
+	 * retrieved along side their matching Callback.
+	 */
+        for (Callback cb : cbs) {
+            if (cb instanceof ChoiceCallback) {
+                ChoiceCallback ccb = (ChoiceCallback) cb;
+                validationPanel.add(new JLabel(ccb.getPrompt()));
+                JComboBox combo = new JComboBox(ccb.getChoices());
+                combo.setSelectedItem(ccb.getDefaultChoice());
+                validationPanel.add(combo);
+                dataFields.add(combo);
+            } else if (cb instanceof ConfirmationCallback) {
+                ConfirmationCallback ccb = (ConfirmationCallback) cb;
+                validationPanel.add(new JLabel(ccb.getPrompt()));
+                JComboBox combo = new JComboBox(ccb.getOptions());
+                combo.setSelectedItem(ccb.getDefaultOption());
+                validationPanel.add(combo);
+                dataFields.add(combo);
+            } else if (cb instanceof NameCallback) {
+                NameCallback ncb = (NameCallback) cb;
+                validationPanel.add(new JLabel(ncb.getPrompt()));
+                JTextField nameField = new JTextField(ncb.getDefaultName());
+                validationPanel.add(nameField);
+                dataFields.add(nameField);
+            } else if (cb instanceof PasswordCallback) {
+                PasswordCallback ncb = (PasswordCallback) cb;
+                validationPanel.add(new JLabel(ncb.getPrompt()));
+                JPasswordField passwordField = new JPasswordField();
+                validationPanel.add(passwordField);
+                dataFields.add(passwordField);
+            } else if (cb instanceof TextInputCallback) {
+                TextInputCallback tcb = (TextInputCallback) cb;
+                validationPanel.add(new JLabel(tcb.getPrompt()));
+                JTextField textField = new JTextField(tcb.getDefaultText());
+                validationPanel.add(textField);
+                dataFields.add(textField);
+            } else if (cb instanceof TextOutputCallback) {
+                TextOutputCallback tcb = (TextOutputCallback) cb;
+                validationPanel.add(new JLabel(tcb.getMessage()));
+            }
+        }
+        pack();
+        setVisible(true);
+    }
+
+    /**
+     * Called when the validation button is pressed.  It iterates
+     * through the array of Callbacks and takes the data from the
+     * matching UI component and sets it in the Callback.
+     */
+    protected void transcribeToCallbacks() {
+        Iterator iter = dataFields.iterator();
+        for (Callback cb : callbacks) {
+            if (cb instanceof ChoiceCallback) {
+                // note this is really wrong, should allow for multiple
+                // select
+                ChoiceCallback ccb = (ChoiceCallback) cb;
+                JComboBox combo = (JComboBox) iter.next();
+                ccb.setSelectedIndex(combo.getSelectedIndex());
+            } else if (cb instanceof ConfirmationCallback) {
+                ConfirmationCallback ccb = (ConfirmationCallback) cb;
+                JComboBox combo = (JComboBox) iter.next();
+                ccb.setSelectedIndex(combo.getSelectedIndex());
+            } else if (cb instanceof NameCallback) {
+                NameCallback ncb = (NameCallback) cb;
+                JTextField nameField = (JTextField) iter.next();
+                ncb.setName(nameField.getText());
+            } else if (cb instanceof PasswordCallback) {
+                PasswordCallback ncb = (PasswordCallback) cb;
+                JPasswordField passwordField = (JPasswordField) iter.next();
+                ncb.setPassword(passwordField.getPassword());
+            } else if (cb instanceof TextInputCallback) {
+                TextInputCallback tcb = (TextInputCallback) cb;
+                JTextField textField = (JTextField) iter.next();
+                tcb.setText(textField.getText());
+            } else if (cb instanceof TextOutputCallback) {
+                // no response required
+            }
+        }
+    }
+}
