3398aff5a7057dd9faa0eb0d207a2c959d73deb6
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-07-23 22:15:19 +0000

    updated with reference to event queue

--- src/com/sun/sgs/service/TaskService.java
+++ src/com/sun/sgs/service/TaskService.java
@@ -4,9 +4,12 @@ package com.sun.sgs.service;
 import com.sun.sgs.ManagedReference;
 import com.sun.sgs.ManagedRunnable;
 
+import com.sun.sgs.kernel.EventQueue;
+
 
 /**
- * This type of <code>Service</code> handles queuing tasks.
+ * This type of <code>Service</code> handles queuing tasks. All services
+ * that need to queue a task do so through a <code>TaskService</code>.
  *
  * @since 1.0
  * @author James Megquier
@@ -14,6 +17,14 @@ import com.sun.sgs.ManagedRunnable;
  */
 public interface TaskService extends Service
 {
+
+    /**
+     * Tells this service about the <code>EventQueue</code> where future
+     * tasks get queued.
+     *
+     * @param eventQueue the system's event queue
+     */
+    public void setEventQueue(EventQueue eventQueue);
 
     /**
      * Queues a task to run.
