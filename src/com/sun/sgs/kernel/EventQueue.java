14e8227d57d01c598ec1b2bafda86b77a3275d5f
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-07-23 22:24:28 +0000

    updated with test logic to try the system as a whole

--- src/com/sun/sgs/kernel/EventQueue.java
+++ src/com/sun/sgs/kernel/EventQueue.java
@@ -4,25 +4,108 @@ package com.sun.sgs.kernel;
 import com.sun.sgs.ManagedRunnable;
 import com.sun.sgs.Quality;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 
 /**
- * This interface is the entry point for all events in the system. 
+ * This class is the entry point for all events in the system.
  *
  * @since 1.0
  * @author James Megquier
  * @author Seth Proctor
  */
-public interface EventQueue
+public class EventQueue
 {
+
+    // the system's resource coordinator
+    private ResourceCoordinator resourceCoordinator;
+
+    // the queue that lines up events
+    private ConcurrentLinkedQueue<Task> eventQueue;
+
+    /**
+     * FIXME: figure out if the pool size is just for testing or actually
+     * a useful parameter.
+     */
+    public EventQueue(ResourceCoordinator resourceCoordinator,
+                      int initialPoolSize) {
+        this.resourceCoordinator = resourceCoordinator;
+
+        // create a simple queue, just for testing
+        eventQueue = new ConcurrentLinkedQueue<Task>();
+
+        // now ask for some number of threads to consume the queue
+        for (int i = 0; i < initialPoolSize; i++) {
+            Task queueTask = new Task(new EventQueueRunnable(), null);
+            resourceCoordinator.requestThread().executeTask(queueTask);
+        }
+    }
 
     /**
      * Queue an event to run when the resources are available.
-     * <p>
-     * FIXME: This also takes some meta-data.
      *
-     * @param r the task to run
-     * @param quality the desired quality of service parameters
+     * @param task the <code>Task</code> to run
      */
-    public void queueEvent(ManagedRunnable r, Quality quality);
+    public void queueEvent(Task task) {
+        // add the new task to the queue
+        eventQueue.add(task);
+
+        // make sure someone is awake to process this task
+        synchronized(eventQueue) {
+            eventQueue.notify();
+        }
+    }
+
+    /**
+     * This <code>Runnable</code> is used to consume tasks off the queue
+     * and execute them.
+     * <p>
+     * NOTE: This is internal right now for testing...although it may
+     * make sense to keep it here, given that no one else need know how
+     * events are processed.
+     */
+    private class EventQueueRunnable implements Runnable {
+        
+        // flag noting whether this thread is still supposed to process tasks
+        private boolean stillProcessing = true;
+
+        /**
+         * Runs the task.
+         */
+        public void run() {
+            while (stillProcessing) {
+                // try to get a task off the queue
+                Task task = eventQueue.poll();
+                
+                if (task != null) {
+                    // we got a task, so run it
+                    task.run();
+                } else {
+                    // there was nothing available, so wait until there is
+                    // something to process
+                    try {
+                        synchronized(eventQueue) {
+                            eventQueue.wait();
+                        }
+                    } catch (InterruptedException ie) {
+                        // FIXME: we don't know yet what to do with this
+                        System.err.println("EventQueue thread interrupted");
+                    }
+                }
+            }            
+        }
+
+        /**
+         * Stops this thread from further processing.
+         * <p>
+         * FIXME: this isn't implemented yet, because it's not clear what
+         * the right interruption and hand-off semantics will be for the
+         * task threads, and for now we don't need this method.
+         */
+        public void stopProcessing() {
+            
+        }
+
+    }
 
 }
--- src/com/sun/sgs/kernel/impl/SimpleTransactionCoordinator.java
+++ src/com/sun/sgs/kernel/impl/SimpleTransactionCoordinator.java
@@ -5,6 +5,8 @@ import com.sun.sgs.Quality;
 
 import com.sun.sgs.kernel.TransactionCoordinator;
 
+import com.sun.sgs.service.DataService;
+import com.sun.sgs.service.TaskService;
 import com.sun.sgs.service.Transaction;
 
 import com.sun.sgs.service.impl.SimpleTransaction;
@@ -26,11 +28,26 @@ public class SimpleTransactionCoordinator implements TransactionCoordinator {
     // generator for the transaction serial identifiers
     private AtomicLong serialGenerator;
 
+    // TEST: an implementation of DataService
+    private DataService dataService;
+
+    // TEST: an implementation of TaskService
+    private TaskService taskService;
+
     /**
      * Creates a new instance of <code>SimpleTransactionCoordinator</code>.
+     * <p>
+     * The parameters here is just for testing purposes...obviously this
+     * would actually come from another facility, or would be provided
+     * to this class, through a factory, with all the other available
+     * services (more likely the former).
      */
-    public SimpleTransactionCoordinator() {
+    public SimpleTransactionCoordinator(DataService dataService,
+                                        TaskService taskService) {
         serialGenerator = new AtomicLong(0);
+
+        this.dataService = dataService;
+        this.taskService = taskService;
     }
 
     /**
@@ -46,7 +63,7 @@ public class SimpleTransactionCoordinator implements TransactionCoordinator {
         // or so that a reference to the coordinator is provided which
         // in turn provides these values
         return new SimpleTransaction(serialGenerator.getAndIncrement(),
-                                     null, null, null, null);
+                                     null, dataService, taskService, null);
     }
 
 }
