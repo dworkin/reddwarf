5070763b4334c7aa7c739da5e6520f97ff97f792
jeffpk <jeffpk@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-08-21 17:45:24 +0000

    Fixed DSOism

--- resources/startSGSWithJME.sh
+++ resources/startSGSWithJME.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec java -cp ./SunGameServer.jar;lib/javax.servlet.jar;lib/org.mortbay.jetty.jar;lib/commons-logging.jar -Dsgs.framework.installurl=file:SGS-appsJME.conf -Djava.util.logging.config.file=logging.properties com.sun.gi.SGS
+exec java -cp ./SunGameServer.jar:lib/javax.servlet.jar:lib/org.mortbay.jetty.jar:lib/commons-logging.jar -Dsgs.framework.installurl=file:SGS-appsJME.conf -Djava.util.logging.config.file=logging.properties com.sun.gi.SGS
+
