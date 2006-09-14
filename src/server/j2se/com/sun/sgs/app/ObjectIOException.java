8450e9dad3a8329c1fd97c86f34d35e1b6f1c264
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-09-14 17:08:44 +0000

    M .
    - Set the svn:ignore property to build, to ignore the build directory
      when showing status.
    
    A src/server/j2se/com/sun/sgs/app/ExceptionRetryStatus.java
    A src/server/j2se/com/sun/sgs/app/NameNotBoundException.java
    A src/server/j2se/com/sun/sgs/app/ObjectIOException.java
    A src/server/j2se/com/sun/sgs/app/ObjectNotFoundException.java
    A src/server/j2se/com/sun/sgs/app/TransactionAbortedException.java
    A src/server/j2se/com/sun/sgs/app/TransactionConflictException.java
    A src/server/j2se/com/sun/sgs/app/TransactionException.java
    A src/server/j2se/com/sun/sgs/app/TransactionNotActiveException.java
    A src/server/j2se/com/sun/sgs/app/TransactionTimeoutException.java
    
    - Added new exception classes that were hashed out as part of the
      DataManager API.

---
+++ src/server/j2se/com/sun/sgs/app/ObjectIOException.java
@@ -0,0 +1,68 @@
+package com.sun.sgs.app;
+
+import java.io.IOException;
+
+/**
+ * Thrown when an operation fails because of an I/O failure when attempting to
+ * access a managed object.  Typically the exception returned by the {@link
+ * #getCause getCause} method will be the {@link IOException} that caused the
+ * failure.
+ */
+public class ObjectIOException extends RuntimeException
+    implements ExceptionRetryStatus
+{
+    /** The version of the serialized form. */
+    private static final long serialVersionUID = 1;
+
+    /**
+     * Whether an operation that throws this exception should
+     * be retried.
+     *
+     * @serial
+     */
+    private final boolean shouldRetry;
+
+    /**
+     * Creates an instance of this class with the specified detail message and
+     * whether an operation that throws this exception should be retried.
+     *
+     * @param	message the detail message or <code>null</code>
+     * @param	shouldRetry whether an operation that throws this exception
+     *		should be retried
+     */
+    public ObjectIOException(String message, boolean shouldRetry) {
+	super(message);
+	this.shouldRetry = shouldRetry;
+    }
+
+    /**
+     * Creates an instance of this class with the specified detail message, the
+     * cause, and whether an operation that throws this exception should be
+     * retried.
+     *
+     * @param	message the detail message or <code>null</code>
+     * @param	cause the cause or <code>null</code>
+     * @param	shouldRetry whether an operation that throws this exception
+     *		should be retried
+     */
+    public ObjectIOException(
+	String message, Throwable cause, boolean shouldRetry)
+    {
+	super(message, cause);
+	this.shouldRetry = shouldRetry;
+    }
+
+    /* -- Implement ExceptionRetryStatus -- */
+
+    /**
+     * {@inheritDoc} <p>
+     *
+     * This implementation returns the value that was specified for the
+     * <code>shouldRetry</code> parameter in the constructor.
+     */
+    public boolean shouldRetry() {
+	return shouldRetry;
+    }
+}
+    
+    
---
+++ src/server/j2se/com/sun/sgs/app/TransactionTimeoutException.java
@@ -0,0 +1,31 @@
+package com.sun.sgs.app;
+
+/**
+ * Thrown when an operation fails because the system aborted the current
+ * transaction when it exceeded the maximum permitted duration.
+ */
+public class TransactionTimeoutException extends TransactionAbortedException {
+
+    /** The version of the serialized form. */
+    private static final long serialVersionUID = 1;
+
+    /**
+     * Creates an instance of this class with the specified detail message.
+     *
+     * @param	message the detail message or <code>null</code>
+     */
+    public TransactionTimeoutException(String message) {
+	super(message);
+    }
+
+    /**
+     * Creates an instance of this class with the specified detail message and
+     * cause.
+     *
+     * @param	message the detail message or <code>null</code>
+     * @param	cause the cause or <code>null</code>
+     */
+    public TransactionTimeoutException(String message, Throwable cause) {
+	super(message, cause);
+    }
+}
