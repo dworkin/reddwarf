24161de9c0c635c6cd10e1539136ab52b21f53d5
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-10-04 18:43:14 +0000

    - Change 'system binding' to 'service binding' to make clear that these   extra bindings are for use by services, but are still within the   context of a particular application.

--- src/server/j2se/com/sun/sgs/service/DataService.java
+++ src/server/j2se/com/sun/sgs/service/DataService.java
@@ -11,18 +11,18 @@ import java.io.Serializable;
 /**
  * Provides facilities for services to manage access to shared, persistent
  * objects.  In addition to the methods provided by {@link DataManager}, this
- * interface includes methods for an additional set of system bindings.  These
- * system bindings are intended to by used by services to support their needs
- * to store objects persistently.  The objects associated with system bindings
+ * interface includes methods for an additional set of service bindings.  These
+ * service bindings are intended to by used by services to support their needs
+ * to store objects persistently.  The objects associated with service bindings
  * are independent of the objects bound through methods defined on
  * <code>DataManager</code>.  Services should insure that they use unique names
- * for system bindings by using their class or package name as the prefix for
+ * for service bindings by using their class or package name as the prefix for
  * the name.
  */
 public interface DataService extends DataManager, Service {
 
     /**
-     * Obtains the object associated with the system binding of a name.
+     * Obtains the object associated with the service binding of a name.
      * Callers need to notify the system before modifying the object or any of
      * the non-managed objects it refers to by calling {@link #markForUpdate
      * markForUpdate} or {@link ManagedReference#getForUpdate
@@ -31,7 +31,7 @@ public interface DataService extends DataManager, Service {
      * @param	<T> the type of the object
      * @param	name the name
      * @param	type a class representing the type of the object
-     * @return	the object associated with the system binding of the name
+     * @return	the object associated with the service binding of the name
      * @throws	ClassCastException if the object bound to the name is not of
      *		the specified type
      * @throws	NameNotBoundException if no object is bound to the name
@@ -40,10 +40,10 @@ public interface DataService extends DataManager, Service {
      * @throws	TransactionException if the operation failed because of a
      *		problem with the current transaction
      */
-    <T extends ManagedObject> T getSystemBinding(String name, Class<T> type);
+    <T extends ManagedObject> T getServiceBinding(String name, Class<T> type);
 
     /**
-     * Specifies an object for the system binding of a name, replacing any
+     * Specifies an object for the service binding of a name, replacing any
      * previous binding.  The object, as well as any objects it refers to, must
      * implement {@link Serializable}.  Note that this method will throw {@link
      * IllegalArgumentException} if <code>object</code> does not implement
@@ -54,17 +54,17 @@ public interface DataService extends DataManager, Service {
      * instances of {@link ManagedReference}.
      *
      * @param	name the name
-     * @param	object the object associated with the system binding of the
+     * @param	object the object associated with the service binding of the
      *		name
      * @throws	IllegalArgumentException if <code>object</code> does not
      *		implement {@link Serializable}
      * @throws	TransactionException if the operation failed because of a
      *		problem with the current transaction
      */
-    void setSystemBinding(String name, ManagedObject object);
+    void setServiceBinding(String name, ManagedObject object);
 
     /**
-     * Removes the system binding for a name.  Note that the object previously
+     * Removes the service binding for a name.  Note that the object previously
      * bound to the name, if any, is not removed; only the binding between the
      * name and the object is removed.  To remove the object, use the {@link
      * #removeObject removeObject} method.
@@ -75,5 +75,5 @@ public interface DataService extends DataManager, Service {
      *		problem with the current transaction
      * @see	#removeObject removeObject
      */
-    void removeSystemBinding(String name);
+    void removeServiceBinding(String name);
 }
