468e287912c22a336a7eb4440bb983723b1648a0
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-10-25 19:12:39 +0000

    This commit adds a start() method on RecurringTaskHandle. This is so that a recurring task can be scheduled during a transaction, but not started until the transaction commits. This is needed so that a PeriodicTaskHandle can be passed back from the DataManager without having to be updated later with the associated RecurringTaskHandle.

--- src/server/j2se/com/sun/sgs/kernel/RecurringTaskHandle.java
+++ src/server/j2se/com/sun/sgs/kernel/RecurringTaskHandle.java
@@ -12,10 +12,20 @@ public interface RecurringTaskHandle
 {
 
     /**
-     * Cancels the associated recurring task.
+     * Cancels the associated recurring task. A recurring task may be
+     * cancelled before it is started.
      *
      * @throws IllegalStateException if the task has already been cancelled
      */
     public void cancel();
+
+    /**
+     * Starts the associated recurring task. A recurring task will not start
+     * running until this method is called.
+     *
+     * @throws IllegalStateException if the task has already been started,
+     *                               or has been cancelled
+     */
+    public void start();
 
 }
