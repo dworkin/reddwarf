33a65d76085ff98cb2722886957fb4ab04781f6a
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-12-08 20:37:20 +0000

    Added a simple utility for adding entried to a password file.

---
+++ src/server/j2se/com/sun/sgs/impl/auth/PasswordFileEditor.java
@@ -0,0 +1,46 @@
+
+package com.sun.sgs.impl.auth;
+
+import com.sun.sgs.impl.auth.NamePasswordAuthenticator;
+
+import java.io.FileOutputStream;
+
+import java.security.MessageDigest;
+
+
+/**
+ * This is a simple utility program used to create the password files that
+ * are consumed by <code>NamePasswordAuthenticator</code>. The password
+ * files consist of one entry per line, where each entry has a name, some
+ * whitespace, a SHA-256 hashed password encoded vi a call to
+ * <code>NamePasswordAuthenticator.encodeBytes</code>, and finally a newline.
+ *
+ * @since 1.0
+ * @author Seth Proctor
+ */
+public class PasswordFileEditor
+{
+
+    /**
+     * Main-line for this utility.
+     */
+    public static void main(String [] args) throws Exception {
+        if (args.length != 3) {
+            System.out.println("Usage: password_file name password");
+            return;
+        }
+
+        // make sure we can hash and encode the password
+        byte [] pass = MessageDigest.getInstance("SHA-256").
+            digest(args[2].getBytes());
+        byte [] encodedPass = NamePasswordAuthenticator.encodeBytes(pass);
+
+        // open the file and append the new entry
+        FileOutputStream out = new FileOutputStream(args[0], true);
+        out.write(args[1].getBytes());
+        out.write("\t".getBytes());
+        out.write(encodedPass);
+        out.write("\n".getBytes());
+    }
+
+}
