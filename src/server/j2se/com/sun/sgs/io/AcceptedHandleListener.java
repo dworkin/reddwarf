8126d6c29e2b79bb02d0fd1281880f89177b0cc4
sten <sten@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-12-14 22:05:09 +0000

    Changed method signature of newHandle to return an IOHandler

--- src/server/j2se/com/sun/sgs/io/AcceptedHandleListener.java
+++ src/server/j2se/com/sun/sgs/io/AcceptedHandleListener.java
@@ -2,7 +2,6 @@ package com.sun.sgs.io;
 
 /**
  * Listens for events on an associated <code>IOAcceptor</code>.
- * Right now the only event is a new, incoming <code>IOHandle</code>.
  * 
  * @author Sten Anderson
  * @since 1.0
@@ -13,8 +12,11 @@ public interface AcceptedHandleListener {
      * Called when a new incoming <code>IOHandle</code> is established. 
      * 
      * @param handle         the incoming <code>IOHandle</code>
+     * 
+     * @return the {@code IOHandler} which will receive events for this 
+     *         {@code IOHandle}
      */
-    public void newHandle(IOHandle handle);
+    public IOHandler newHandle(IOHandle handle);
 
     
 
