87ffe0e5fcfad904fa18451ea3c5dcd6ebfb9172
sten <sten@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2006-12-14 22:08:57 +0000

    Refactored to be thread-safe

--- src/server/j2se/com/sun/sgs/impl/io/SocketHandler.java
+++ src/server/j2se/com/sun/sgs/impl/io/SocketHandler.java
@@ -5,39 +5,29 @@ import org.apache.mina.common.IdleStatus;
 import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoSession;
 
+import com.sun.sgs.io.IOFilter;
 import com.sun.sgs.io.IOHandle;
 import com.sun.sgs.io.IOHandler;
 
 /**
  *  This is an adapter between an Apache Mina {@link IoHandler} and the SGS
- *  IO framework {@link IOHandler}.  This is used on the client side to 
- *  pass messages from Mina onto the associated {@code IOHandler}.
+ *  IO framework {@link IOHandler}.  SocketHandlers exist one per handle on
+ *  the client {@code IOConnector} side, and exit one per {@code IOAcceptor}
+ *  on the server side.
  * 
  * @author      Sten Anderson
  * @version     1.0
  */
 public class SocketHandler implements IoHandler {
     
-    private IOHandler handler;
-    
-    /**
-     * Constructs a new {@code SocketHandler}.  As callbacks get fired from
-     * the Apache Mina framework, they are forwarded on to the given
-     * {@code IOHandler}.
-     * 
-     * @param handler           the {@code IOHandler} that will receive 
-     *                          callbacks.
-     */
-    public SocketHandler(IOHandler handler) {
-        this.handler = handler;
-    }
 
     /**
      * Called by the Mina framework when the given session is closed.  Forward
      * this onto the associated handler as "disconnected".
      */
     public void sessionClosed(IoSession session) throws Exception {
-        handler.disconnected(getHandle(session));
+        SocketHandle handle = (SocketHandle) session.getAttachment();
+        handle.getIOHandler().disconnected(handle);
     }
 
     /**
@@ -51,32 +41,30 @@ public class SocketHandler implements IoHandler {
     public void exceptionCaught(IoSession session, Throwable exception)
                                                             throws Exception {
         
-        handler.exceptionThrown(exception, getHandle(session));
-        
+        SocketHandle handle = (SocketHandle) session.getAttachment();
+        handle.getIOHandler().exceptionThrown(exception, handle);
     }
 
     /**
      * Called by the Mina framework when some amount of data comes in on the 
      * given session.  The data is packed into a byte array and forwarded to
-     * the associated handler.
+     * the associated filter.  The filter decides if and how to forward the
+     * data on to the associated handler.
      * 
      * @param session           the session on which the data has arrived
-     * @param message           the data, which in practice is a mina ByteBuffer
+     * @param message           the data, which in practice is a Mina ByteBuffer
      */
     public void messageReceived(IoSession session, Object message) throws Exception {
+        SocketHandle handle = (SocketHandle) session.getAttachment();
+        
         ByteBuffer minaBuffer = (ByteBuffer) message;
         
         byte[] array = new byte[minaBuffer.remaining()];
         minaBuffer.get(array);
-        handler.messageReceived(array, getHandle(session));
         
+        handle.getFilter().filterReceive(handle, array);
     }
 
-    private IOHandle getHandle(IoSession session) {
-        return (IOHandle) session.getAttachment();
-    }
-    
-    
     // these Mina call-backs aren't used.
     public void sessionCreated(IoSession session) throws Exception {}
 
