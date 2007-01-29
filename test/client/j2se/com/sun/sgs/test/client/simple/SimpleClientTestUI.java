7fd07337cea819ca255216455f8afac72eb13748
jm199043 <jm199043@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-01-29 22:59:02 +0000

    Remove unused imports missed on earlier commit.

--- test/client/j2se/com/sun/sgs/test/client/simple/SimpleClientTestUI.java
+++ test/client/j2se/com/sun/sgs/test/client/simple/SimpleClientTestUI.java
@@ -8,8 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.PasswordAuthentication;
 import java.util.Properties;
