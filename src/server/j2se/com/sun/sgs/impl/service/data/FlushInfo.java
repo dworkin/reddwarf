4753aa90bf7863bd2816e30e8b39ae368dba220b
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-01-29 20:41:42 +0000

    - Forgot to add these new files.

---
+++ src/server/j2se/com/sun/sgs/impl/service/data/FlushInfo.java
@@ -0,0 +1,57 @@
+package com.sun.sgs.impl.service.data;
+
+/**
+ * Stores information about objects that have been modified and need their data
+ * stored.
+ */
+final class FlushInfo {
+    /** Space to allocate for buffering results. */
+    private static final int BUFFER = 50;
+
+    /** The object IDs of modified objects. */
+    private long[] oids = new long[BUFFER];
+
+    /** The associated data. */
+    private byte[][] dataArray = new byte[BUFFER][];
+
+    /** The number of modified objects collected. */
+    private int count = 0;
+
+    /** Creates an instance. */
+    FlushInfo() { }
+
+    /** Adds the object ID and data for a modified object. */
+    void add(long oid, byte[] data) {
+	if (count == oids.length) {
+	    long[] newOids = new long[count + BUFFER];
+	    System.arraycopy(oids, 0, newOids, 0, count);
+	    oids = newOids;
+	    byte[][] newDataArray = new byte[count + BUFFER][];
+	    System.arraycopy(dataArray, 0, newDataArray, 0, count);
+	    dataArray = newDataArray;
+	}
+	oids[count] = oid;
+	dataArray[count] = data;
+	count++;
+    }
+
+    /** Returns the object IDs of the modified objects. */
+    long[] getOids() {
+	if (count == oids.length) {
+	    return oids;
+	}
+	long[] result = new long[count];
+	System.arraycopy(oids, 0, result, 0, count);
+	return result;
+    }
+
+    /** Returns the data of the modified objects. */
+    byte[][] getDataArray() {
+	if (count == dataArray.length) {
+	    return dataArray;
+	}
+	byte[][] result = new byte[count][];
+	System.arraycopy(dataArray, 0, result, 0, count);
+	return result;
+    }
+}
---
+++ test/server/j2se/com/sun/sgs/test/impl/service/data/store/net/TestDataServiceConcurrentClient.java
@@ -0,0 +1,69 @@
+package com.sun.sgs.test.impl.service.data.store.net;
+
+import com.sun.sgs.impl.service.data.DataServiceImpl;
+import com.sun.sgs.impl.service.data.store.DataStore;
+import com.sun.sgs.impl.service.data.store.net.DataStoreClient;
+import com.sun.sgs.impl.service.data.store.net.DataStoreServerImpl;
+import com.sun.sgs.kernel.ComponentRegistry;
+import com.sun.sgs.service.DataService;
+import com.sun.sgs.test.impl.service.data.TestConcurrent;
+import java.util.Properties;
+
+public class TestDataServiceConcurrentClient extends TestConcurrent {
+    private static boolean externalServer =
+	Boolean.getBoolean("test.externalServer");
+    private static String serverHost =
+	System.getProperty("test.serverHost", "localhost");
+    private static int serverPort =
+	Integer.getInteger("test.serverPort", 54321);
+    
+    /** The name of the DataStoreClient class. */
+    private static final String DataStoreClientClassName =
+	DataStoreClient.class.getName();
+
+    /** The name of the DataStoreServerImpl class. */
+    private static final String DataStoreServerImplClassName =
+	DataStoreServerImpl.class.getName();
+
+    private static final String DataServiceImplClassName =
+	DataServiceImpl.class.getName();
+
+    DataStoreServerImpl server;
+
+    public TestDataServiceConcurrentClient(String name) {
+	super(name);
+    }
+
+    protected void tearDown() throws Exception {
+	super.tearDown();
+	try {
+	    if (server != null) {
+		server.shutdown();
+	    }
+	} catch (RuntimeException e) {
+	    if (passed) {
+		throw e;
+	    } else {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    protected DataService getDataService(Properties props,
+					 ComponentRegistry componentRegistry)
+	throws Exception
+    {
+	if (!externalServer) {
+	    props.setProperty(DataStoreServerImplClassName + ".port", "0");
+	    DataStoreServerImpl serverImpl = new DataStoreServerImpl(props);
+	    server = serverImpl;
+	    serverPort = serverImpl.getPort();
+	}
+	props.setProperty(DataStoreClientClassName + ".host", serverHost);
+	props.setProperty(DataStoreClientClassName + ".port",
+			  String.valueOf(serverPort));
+	props.setProperty(DataServiceImplClassName + ".dataStoreClass",
+			    DataStoreClientClassName);
+	return new DataServiceImpl(props, componentRegistry);
+    }
+}
