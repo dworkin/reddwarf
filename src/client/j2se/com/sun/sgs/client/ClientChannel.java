0d051c40383f7547fcf5b6a3cf8724e0d555eeb3
sp76946 <sp76946@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-01-30 22:17:22 +0000

    Continued update to trunk@1571-1600. This adds the client files that were not committed in the bulk commit.

--- src/client/j2se/com/sun/sgs/client/ClientChannel.java
+++ src/client/j2se/com/sun/sgs/client/ClientChannel.java
@@ -1,98 +1,102 @@
 package com.sun.sgs.client;
 
-import java.util.Collection;
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * Represents a client's view of a channel.  A channel is a
  * communication group, consisting of multiple clients and the server.
- *
- * <p>The server is solely responsible for creating channels and
+ * <p>
+ * The server is solely responsible for creating channels and
  * adding and removing clients from channels.  If desired, a client
  * can request that a channel be created by sending an
  * application-specific message to the server (using its {@link
  * ServerSession}).
- *
- * <p>When the server adds a client session to a channel, the client's
+ * <p>
+ * When the server adds a client session to a channel, the client's
  * {@link ServerSessionListener}'s {@link
  * ServerSessionListener#joinedChannel joinedChannel} method is
  * invoked with that client channel, returning the client's
- * <code>ClientChannelListener</code> for the channel.  A
- * <code>ClientChannelListener</code> for a client channel is notified
- * as follows: <ul>
- *
+ * {@code ClientChannelListener} for the channel.  A
+ * {@code ClientChannelListener} for a client channel is notified
+ * as follows:
+ * <ul>
  * <li>When a message is received on a client channel, the listener's
  * {@link ClientChannelListener#receivedMessage receivedMessage}
  * method is invoked with the channel, the sender's session identifier,
- * and the message.  A <code>null</code> sender indicates that the
+ * and the message.  A {@code null} sender indicates that the
  * message was sent by the server.  The listener is <i>not</i>
  * notified of messages that its client sends on its associated
- * channel.
+ * channel.</li>
  *
  * <li> When the associated client leaves a channel, the listener's
  * {@link ClientChannelListener#leftChannel leftChannel} method is
  * invoked with the channel.  Once a client has been removed
  * from a channel, that client can no longer send messages on that
- * channel.
+ * channel.</li>
  * </ul>
  */
 public interface ClientChannel {
 
     /**
-     * Returns the name of this channel, which is the name that the
-     * server assigned to the channel.
+     * Returns the name of this channel.  A channel's name is set when
+     * it is created by the server-side application.
      *
      * @return the name of this channel
      */
     String getName();
-    
-    /**
-     * Sends the message contained in the specified byte array to all
-     * clients joined to this channel.  If no clients are joined to
-     * this channel, then no action is taken.
-     *
-     * <p>The specified byte array must not be modified after invoking
-     * this method; if the byte array is modified, then this method
-     * may have unpredictable results.
-     *
-     * @param message a message
-     *
-     * @throws IllegalStateException if the associated client has been
-     * removed from this channel
-     */
-    void send(byte[] message);
 
     /**
-     * Sends the message contained in the specified byte array to the
-     * specified recipient.  If the specified recipient is not joined
-     * to this channel, then no action is taken.
-     *
-     * <p>The specified byte array must not be modified after invoking
+     * Sends the given {@code message} to all channel members.  If this
+     * channel has no members other than the sender, then no action is taken.
+     * <p>
+     * The specified byte array must not be modified after invoking
      * this method; if the byte array is modified, then this method
      * may have unpredictable results.
      *
-     * @param recipient the session identifier of a recipient
-     * @param message a message
+     * @param message a message to send
      *
-     * @throws IllegalStateException if the associated client has been
-     * removed from this channel
+     * @throws IllegalStateException if the sender is not a member of
+     *         this channel
+     * @throws IOException if a synchronous IO problem occurs
      */
-    void send(SessionId recipient, byte[] message);
+    void send(byte[] message) throws IOException;
 
     /**
-     * Sends the message contained in the specified byte array to the
-     * recipients contained in the specified collection.  Any
-     * specified clients that are not currently joined to the channel
-     * are ignored.
-     *
-     * <p>The specified byte array must not be modified after invoking
+     * Sends the given {@code message} to the {@code recipient} on this
+     * channel.  If the {@code recipient} is not a member of this channel,
+     * then no action is taken.
+     * <p>
+     * The specified byte array must not be modified after invoking
      * this method; if the byte array is modified, then this method
      * may have unpredictable results.
      *
-     * @param recipients a collection of client recipients
-     * @param message a message
+     * @param recipient the channel member that should receive the message
+     * @param message a message to send
      *
-     * @throws IllegalStateException if the associated client has been
-     * removed from this channel
+     * @throws IllegalStateException if the sender is not a member of
+     *         this channel
+     * @throws IOException if a synchronous IO problem occurs
      */
-    void send(Collection<SessionId> recipients, byte[] message);
+    void send(SessionId recipient, byte[] message) throws IOException;
+
+    /**
+     * Sends the given {@code message} data to the specified
+     * {@code recipients} on this channel. Any {@code recipients} that are
+     * not members of this channel are ignored.
+     * <p>
+     * The specified byte array must not be modified after invoking this
+     * method; if the byte array is modified, then this method may have
+     * unpredictable results.
+     * 
+     * @param recipients the subset of channel members that should receive
+     *        the message
+     * @param message a message to send
+     *
+     * @throws IllegalStateException if the sender is not a member of this
+     *         channel
+     * @throws IOException if a synchronous IO problem occurs
+     */
+    void send(Set<SessionId> recipients, byte[] message) throws IOException;
+
 }
