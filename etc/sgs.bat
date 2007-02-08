ed2e002cc7b4cbec4d91b052446bd36c985c3809
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-02-08 15:03:51 +0000

    M build.xml
    - Remove the server.jar property -- this value is now hardcoded.
    - Change the project version to 1.0alpha.
    - Set project.build to the current date, and include it in all of the
      javadoc.
    - Provide more comments about the bdb.lib.dir property and document the
      new bdb.builds.dir property.
    - Modify the all target to depend on build, so that all builds occur,
      not just ones depended on by the jar target, and to add a dependency
      on the new dist target.
    - Modify the build-client target to depend on build-server, since the
      client code seems to depend on server classes.
    - Add the dist, dist-client, dist-server, and dist-bdb targets, for
      building files for distribution.
    - Add the new jar-client target and modify the jar target to depend on
      it.
    - Modify the jar-server target to hardcode the name of the JAR file
      created, and to include a manifest that specifies the main class and
      the class path.
    
    A etc/sgs.bat
    
    - Windows batch file for starting the server
    
    A etc/sgs.config
    
    - Empty placeholder file for configuration of the server.
    
    A etc/sgs.logging
    
    - Logging file for the server
    
    A etc/sgs.sh
    
    - Bourne shell script for starting the server

---
+++ etc/sgs.bat
@@ -0,0 +1,16 @@
+@rem Windows batch file for starting the SGS server
+
+@rem Set this variable to the classpath entries needed to load
+@rem application classes
+
+@set app_classpath=
+
+@rem Set this variable to a space-separated list of application names
+
+@set app_names=""
+
+@rem Run the SGS server, specifying the library path, the logging
+@rem configuration file, the SGS configuration file, the classpath, the
+@rem main class, and the application names.
+
+java -Djava.library.path=lib/bdb/windows-x86 -Djava.util.logging.config.file=sgs.logging -Dcom.sun.sgs.config.file=sgs.config -cp lib/sgs.jar;%app_classpath% com.sun.sgs.impl.kernel.Kernel %app_names%
