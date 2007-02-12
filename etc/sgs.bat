ed83f731eb801a170df7cebb98869a5ba2a9c1bd
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-02-12 20:56:59 +0000

    M build.xml
    - Modify the dist target to only build a zip file, and use the
      zipfileset element to set the mode on sgs.sh.
    - Modify the dist-bdb target use simple fileset patterns to choose the
      library files, so that we can build when running on Windows.
    
    M etc/sgs.bat
    
    - Add comment about needing to run the script from the sgs directory.
    - Clarify that classpath entries should be separated by semicolons.
    - Change app_names to app_config_files.
    
    M etc/sgs.config
    
    - Clarify that this file is for configuration options that apply to all
      applications.
    
    M etc/sgs.sh
    
    - Clarify that classpath entries should use the appropriate path
      separator.
    - Replace app_names with app_config_files.
    - Compute the appropriate path separator.
    - Improve error checking for bad hardware, and fix a typo in the Windows
      case.

--- etc/sgs.bat
+++ etc/sgs.bat
@@ -1,16 +1,18 @@
-@rem Windows batch file for starting the SGS server
+@rem Windows batch file for starting the SGS server -- run this script
+@rem from the sgs directory
 
-@rem Set this variable to the classpath entries needed to load
-@rem application classes
+@rem Set this variable to the semicolon-separated classpath entries
+@rem needed to load application classes
 
 @set app_classpath=
 
-@rem Set this variable to a space-separated list of application names
+@rem Set this variable to a space-separated list of application
+@rem configuration files
 
-@set app_names=
+@set app_config_files=
 
 @rem Run the SGS server, specifying the library path, the logging
 @rem configuration file, the SGS configuration file, the classpath, the
-@rem main class, and the application names.
+@rem main class, and the application configuration files
 
-java -Djava.library.path="%cd%"\lib\bdb\win32-x86 -Djava.util.logging.config.file=sgs.logging -Dcom.sun.sgs.config.file=sgs.config -cp "lib\sgs.jar;%app_classpath%" com.sun.sgs.impl.kernel.Kernel %app_names%
+java -Djava.library.path=lib\bdb\win32-x86 -Djava.util.logging.config.file=sgs.logging -Dcom.sun.sgs.config.file=sgs.config -cp "lib\sgs.jar;%app_classpath%" com.sun.sgs.impl.kernel.Kernel %app_config_files%
--- etc/sgs.sh
+++ etc/sgs.sh
@@ -2,71 +2,86 @@
 # Bourne shell script for starting the SGS server
 
 # Set this variable to the classpath entries needed to load application
-# classes
+# classes.  Note that entries should be separated by the path separator
+# appropriate for current platform.
 app_classpath=
 
-# Set this variable to a space-separated list of application names
-app_names=""
+# Set this variable to a space-separated list of application
+# configuration files
+app_config_files=""
 
-# Figure out what platform we're running on and set the os and mach
-# variables appropriately.  Here are the supported platforms:
+# Figure out what platform we're running on and set the platform and
+# pathsep variables appropriately.  Here are the supported platforms:
 #
-# OS		Hardware	BDB subdirectory
-# --------	--------	----------------
-# Mac OS X	PowerPC		macosx-ppc
-# Mac OS X	Intel x86	macosx-x86
-# Solaris	Intel x86	solaris-x86
-# Solaris	Sparc		solaris-sparc
-# Linux		Intel x86	linux-x86
-# Windows	Intel x86	win32-x86
+# OS		Hardware	Platform	Path Separator
+# --------	--------	--------------	--------------
+# Mac OS X	PowerPC		macosx-ppc	:
+# Mac OS X	Intel x86	macosx-x86	:
+# Solaris	Intel x86	solaris-x86	:
+# Solaris	Sparc		solaris-sparc	:
+# Linux		Intel x86	linux-x86	:
+# Windows	Intel x86	win32-x86	;
 #
 platform=unknown
 os=`uname -s`
 case $os in
     Darwin)
+	pathsep=":"
 	mach=`uname -p`
 	case $mach in
 	    powerpc)
 		platform=macosx-ppc;;
 	    i386)
 	    	platform=macosx-x86;;
+	    *)
+		echo Unknown hardware: $mach;
+		exit 1;
 	esac;;
     SunOS)
+	pathsep=":"
 	mach=`uname -p`
 	case $mach in
 	    i386)
 	    	platform=solaris-x86;;
 	    sparc)
 	    	platform=solaris-sparc;;
+	    *)
+		echo Unknown hardware: $mach;
+		exit 1;
 	esac;;
     Linux)
+	pathsep=":"
 	mach=`uname -m`;
 	case $mach in
 	    i686)
 		platform=linux-x86;;
+	    *)
+		echo Unknown hardware: $mach;
+		exit 1;
 	esac;;
     CYGWIN*)
+	pathsep=";"
 	mach=`uname -m`;
-	case $man in
+	case $mach in
 	    i686)
 		platform=win32-x86;;
+	    *)
+		echo Unknown hardware: $mach;
+		exit 1;
 	esac;;
+    *)
+	echo Unknown operating system: $os;
+	exit 1;
 esac
-
-if test $platform = unknown;
-then
-    echo Unknown operating system: $os;
-    exit 1;
-fi
 
 set -x
 
 # Run the SGS server, specifying the library path, the logging
 # configuration file, the SGS configuration file, the classpath, the
-# main class, and the application names.
+# main class, and the application files
 java -Djava.library.path=lib/bdb/$platform \
      -Djava.util.logging.config.file=sgs.logging \
      -Dcom.sun.sgs.config.file=sgs.config \
-     -cp "lib/sgs.jar:$app_classpath" \
+     -cp "lib/sgs.jar$pathsep$app_classpath" \
      com.sun.sgs.impl.kernel.Kernel \
-     $app_names
+     $app_config_files
