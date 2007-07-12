691464ed3285b8251d1a871011a51e469975c8f6
ir209913 <ir209913@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-07-12 20:11:33 +0000

    Added second operation; in which the length of the (junk) message is specified, instead of the message itself

--- benchmark/src/com/sun/sgs/benchmark/app/impl/SendChannelMessageModule.java
+++ benchmark/src/com/sun/sgs/benchmark/app/impl/SendChannelMessageModule.java
@@ -11,7 +11,7 @@ import com.sun.sgs.app.Channel;
 import com.sun.sgs.app.ChannelManager;
 import com.sun.sgs.app.ClientSession;
 import com.sun.sgs.app.NameNotBoundException;
-
+import com.sun.sgs.app.TransactionException;
 
 import com.sun.sgs.benchmark.app.BehaviorModule;
 
@@ -38,42 +38,72 @@ public class SendChannelMessageModule implements BehaviorModule, Serializable {
      * Fill me in
      */
     public List<Runnable> getOperations(final ClientSession session, Object[] args) {
+        
+        System.out.println("SendChannelMessageModule invoked.");
+        
        	List<Runnable> operations = new LinkedList<Runnable>();
-	if (args.length < 2) {
+	if (args.length != 2) {
 	    System.out.printf("invalid parameter(s) to %s: %s\n",
-			      this, Arrays.toString(args));
+                this, Arrays.toString(args));
 	    return operations;
 	}
-	String channelName = null;
-	String message = null;
+        
+	final String channelName, message;
+        
 	try {
 	    channelName = (String)(args[0]);
-	    message = (String)(args[1]);
-	}
+        }
 	catch (ClassCastException cce) {
 	    System.out.printf("invalid parameter(s) to %s: %s\n" +
-			      "expected java.lang.String\n" ,
-			      this, args[0]);
+                "expected java.lang.String\n", this, args[0]);
 	    return operations;
 	}
-
-	final String name = channelName;
-	final String m = message;
-
+        
+        try {
+            if (args[1] instanceof String) {
+                message = (String)(args[1]);
+            }
+            else {
+                message = junkString((Integer)(args[1]));
+            }
+	}
+	catch (ClassCastException cce) {
+	    System.out.printf("invalid parameter(s) to %s: %s\nexpected" +
+                "java.lang.String or java.lang.Integer\n", this, args[1]);
+	    return operations;
+	}
+        
 	operations.add(new Runnable() {
 		public void run() {
 		    ChannelManager cm = AppContext.getChannelManager();
 		    try {
-			Channel chan = cm.getChannel(name);
-			chan.send(m.getBytes());
+			Channel chan = cm.getChannel(channelName);
+			chan.send(message.getBytes());
 		    } catch (NameNotBoundException nnbe) {
-			System.out.printf("Client tried send message on "
-					  + "non-existent channel: %s", name);
+			System.out.printf("Client requested that server send a" +
+                            " message on non-existent channel: %s", channelName);
+		    } catch (TransactionException te) {
+			System.out.printf("Channel.send() failed due to a " +
+                            "transaction exception: %s\n", te.getMessage());
 		    }
 		}
 	    });
 	return operations;
 
     }
-
+    
+    /*
+     * Returns a {@code String} of the specified length, filled with an
+     * arbitrary character.
+     *
+     * @throws IllegalArgumentException if {@code len} is negative.
+     */
+    private static String junkString(int len) {
+        if (len < 0)
+            throw new IllegalArgumentException("Invalid length: " + len);
+        
+        char[] ca = new char[len];
+        for (int i=0; i < len; i++) ca[i] = 'a';
+        return new String(ca);
+    }
 }
