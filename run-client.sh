68adf8e4b90e559d8ad726c4d671176e153d4504
jm199043 <jm199043@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-09-28 17:50:03 +0000

    create simplified build file for jsr203 based on darkstar build.xml

--- run-client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=xt:jsr203.jar
-export CLASSPATH
-
-exec java -server -Dmaxthreads=16 -Dclients=20000 -Dmessages=1000 -Dhost=dstar2 -d64 -XX:+UseBoundThreads -XX:+MaxFDLimit -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file=client-log.properties -Dcom.sun.sgs.nio.channels.spi.AsynchronousChannelProvider=com.sun.sgs.impl.nio.ReactiveAsyncChannelProvider com.sun.sgs.test.impl.nio.EchoClient
--- run-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=xt:jsr203.jar
-export CLASSPATH
-
-exec java -server -Dmaxthreads=16 -Dbacklog=5 -d64 -XX:+UseBoundThreads -XX:+MaxFDLimit -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file=server-log.properties -Dcom.sun.sgs.nio.channels.spi.AsynchronousChannelProvider=com.sun.sgs.impl.nio.ReactiveAsyncChannelProvider com.sun.sgs.test.impl.nio.EchoServer
