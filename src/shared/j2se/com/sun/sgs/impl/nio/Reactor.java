d7c127a553ee326225507f073119d55b40a23521
jm199043 <jm199043@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-10-08 16:33:16 +0000

    Finish documenting Reactor.

--- src/shared/j2se/com/sun/sgs/impl/nio/Reactor.java
+++ src/shared/j2se/com/sun/sgs/impl/nio/Reactor.java
@@ -11,6 +11,7 @@ import static java.nio.channels.SelectionKey.OP_WRITE;
 import java.io.Closeable;
 import java.io.IOException;
 import java.nio.channels.AlreadyConnectedException;
+import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.ConnectionPendingException;
 import java.nio.channels.NotYetConnectedException;
 import java.nio.channels.SelectableChannel;
@@ -41,7 +42,16 @@ import com.sun.sgs.nio.channels.ShutdownChannelGroupException;
 import com.sun.sgs.nio.channels.WritePendingException;
 
 /**
- * TODO doc
+ * Reactive implementation of the Proactor pattern; an asynchronous IO
+ * dispatcher.  When an asynchronous IO operation is initiated, the reactor
+ * enables interest in that operation with a {@link Selector}, returning
+ * a future that will be completed when the operation becomes ready and the
+ * IO is performed.
+ * <p>
+ * The actual behavior of completing the IO operation is provided by the
+ * asynchronous channel implementations; the reactor merely signals readiness
+ * and invokes the completion handler for the operation as the operations
+ * complete or are cancelled.
  */
 class Reactor {
 
@@ -421,27 +431,38 @@ class Reactor {
     }
 
     /**
-     * Manages a particular IO operation for an async key.
+     * Manages a single asynchronous IO operation for a
+     * {@link ReactiveAsyncKey}. Behaves appropriately when no operation is
+     * pending, or when a race occurs between, e.g., timeout and user
+     * cancellation.
      */
-    class PendingOperation {
+    abstract class PendingOperation {
 
-        /** TODO doc */
+        /**
+         * The continuation of the IO task to perform, if one is pending,
+         * or a reference to {@code null} if an operation is not pending.
+         */
         protected final AtomicReference<AsyncOp<?>> task =
             new AtomicReference<AsyncOp<?>>();
 
-        /** TODO doc */
+        /**
+         * The timeout token for the pending task, or {@code null} if
+         * there is no pending timeout.
+         */
         private volatile TimeoutHandler timeoutHandler = null;
 
-        /** TODO doc */
+        /** The async key. */
         private final ReactiveAsyncKey asyncKey;
 
-        /** TODO doc */
+        /**  The selectable IO operation managed by this instance. */
         private final int op;
 
         /**
-         * TODO doc
-         * @param asyncKey
-         * @param op
+         * Creates a new instance to manage the given operation for the
+         * given key.
+         * 
+         * @param asyncKey the async key
+         * @param op the operation to manage
          */
         PendingOperation(ReactiveAsyncKey asyncKey, int op) {
             this.asyncKey = asyncKey;
@@ -449,12 +470,16 @@ class Reactor {
         }
 
         /**
-         * TODO doc
+         * Overridden by subclasses to take the appropriate action when an
+         * attempt is made to invoke this operation while it is already
+         * pending.
          */
-        protected void pendingPolicy() {}
+        protected abstract void pendingPolicy();
 
         /**
-         * TODO doc
+         * Runs the pending operation, if any.
+         * 
+         * @see AsyncKey#selected(int)
          */
         void selected() {
             Runnable selectedTask = task.getAndSet(null);
@@ -469,53 +494,53 @@ class Reactor {
         }
 
         /**
-         * TODO doc
+         * Returns {@code true} if this operation is pending, otherwise
+         * {@code false}.
          * 
          * @return {@code true} if this operation is pending, otherwise
          *         {@code false}
+         * 
+         * @see AsyncKey#isOpPending(int)
          */
         boolean isPending() {
             return task.get() != null;
         }
 
         /**
-         * TODO doc
-         */
-        void timeoutExpired() {
-            AsyncOp<?> expiredTask = task.getAndSet(null);
-            if (expiredTask == null) {
-                log.log(Level.FINEST,
-                    "timed out but nothing to do {0}", this);
-                return;
-            } else {
-                log.log(Level.FINER, "timeout {0}", this);
-                expiredTask.timeoutExpired();
-            }
-        }
-
-        /**
-         * TODO doc
+         * Cancels the timeout for the pending task, if any, 
          */
         void cleanupTask() {
+            assert task.get() != null;
+
             if (timeoutHandler != null) {
                 try {
                     timeouts.remove(timeoutHandler);
                 } catch (Throwable ignore) { }
                 timeoutHandler = null;
             }
+
             task.set(null);
         }
 
         /**
-         * TODO doc
-         * @param <R>
-         * @param <A>
-         * @param attachment
-         * @param handler
-         * @param timeout
-         * @param unit
-         * @param callable
+         * Attempts to initiate an asynchronous operation.  If an operation
+         * is already pending, an appropriate exception is thrown by a
+         * subclass via its
+         * {@link PendingOperation#pendingPolicy() pendingPolicy()}.
+         * 
+         * @param <R> the result type
+         * @param <A> the attachment type
+         * @param attachment the attachment for the completion handler; may
+         *        be {@code null}
+         * @param handler the completion handler; may be {@code null}
+         * @param timeout the timeout, or {@code 0} indicating no timeout
+         * @param unit the unit of the timeout
+         * @param callable the IO action to perform when this operation is
+         *        ready
          * @return an {@code IoFuture} representing the pending operation
+         * 
+         * @see AsyncKey#execute(int, Object, CompletionHandler, long,
+         *      TimeUnit, Callable)
          */
         <R, A> IoFuture<R, A>
         execute(final A attachment,
@@ -530,24 +555,30 @@ class Reactor {
             AsyncOp<R> opTask = new AsyncOp<R>(callable) {
                 @Override
                 protected void done() {
+                    // Clear the timeout and pending flag
                     cleanupTask();
+                    // Invoke the completion handler, if any
                     if (handler != null) {
                         asyncKey.execute(
                             group.completionRunner(handler, attachment, this));
                     }
                 }};
 
+            // Indicate that a task is pending
             if (! task.compareAndSet(null, opTask))
                 pendingPolicy();
 
+            // Set the timeout handler for the pending task, if any
             if (timeout > 0) {
-                timeoutHandler = new TimeoutHandler(this, timeout, unit);
+                timeoutHandler = new TimeoutHandler(opTask, timeout, unit);
                 timeouts.add(timeoutHandler);
             }
 
             try {
+                // Schedule this task for execution when it is ready
                 Reactor.this.awaitReady(asyncKey, op, opTask);
             } catch (RuntimeException e) {
+                // If a problem occurs, cancel the timeout and pending task
                 cleanupTask();
                 throw e;
             }
@@ -566,35 +597,41 @@ class Reactor {
     }
 
     /**
-     * TODO doc
+     * Provides support for initiating asynchronous IO operations on
+     * an underlying reactive channel registered with some {@link Reactor}.
+     * 
+     * @see AsyncKey
      */
     class ReactiveAsyncKey implements AsyncKey {
 
-        /** TODO doc */
+        /**
+         * The {@link SelectionKey} representing the underlying channel's
+         * registration with this {@code Reactor}'s {@link Selector}.
+         */
         final SelectionKey key;
 
-        /** TODO doc */
+        /** The handler for an asynchronous {@code accept} operation. */
         final PendingOperation pendingAccept =
             new PendingOperation(this, OP_ACCEPT) {
                 protected void pendingPolicy() {
                     throw new AcceptPendingException();
                 }};
 
-        /** TODO doc */
+        /** The handler for an asynchronous {@code connect} operation. */
         final PendingOperation pendingConnect =
             new PendingOperation(this, OP_CONNECT) {
                 protected void pendingPolicy() {
                     throw new ConnectionPendingException();
                 }};
 
-        /** TODO doc */
+        /** The handler for an asynchronous {@code read} operation. */
         final PendingOperation pendingRead =
             new PendingOperation(this, OP_READ) {
                 protected void pendingPolicy() {
                     throw new ReadPendingException();
                 }};
 
-        /** TODO doc */
+        /** The handler for an asynchronous {@code write} operation. */
         final PendingOperation pendingWrite = 
             new PendingOperation(this, OP_WRITE) {
                 protected void pendingPolicy() {
@@ -602,8 +639,10 @@ class Reactor {
                 }};
 
         /**
-         * TODO doc
-         * @param key
+         * Creates a new instance that wraps the given selector key.
+         * 
+         * @param key the selection key that represents the underlying
+         *        channel's registration with this reactor's selector
          */
         ReactiveAsyncKey(SelectionKey key) {
             this.key = key;
@@ -611,12 +650,26 @@ class Reactor {
 
         /**
          * {@inheritDoc}
+         * <p>
+         * This implemenation does the following:
+         * <ul>
+         * <li>
+         * Unregisters the underlying channel with the reactor
+         * <li>
+         * Closes the channel
+         * <li>
+         * Awakens any pending asynchronous operations so they can complete
+         * with {@link AsynchronousCloseException} when they notice the
+         * channel is closed.
+         * </ul>
          */
         public void close() throws IOException {
             log.log(Level.FINER, "closing {0}", this);
             if (! key.isValid()) {
                 log.log(Level.FINE, "key is already invalid {0}", this);
             }
+            // TODO currently we call unregister() to cancel the selection
+            // key before closing the channel, but is this neccessary? -JM
             Reactor.this.unregister(this);
             try {
                 key.channel().close();
@@ -654,6 +707,7 @@ class Reactor {
          * {@inheritDoc}
          */
         public void selected(int readyOps) {
+            // Dispatch writes first in hopes of reducing roundtrip latency
             if ((readyOps & OP_WRITE) != 0)
                 pendingWrite.selected();
             if ((readyOps & OP_READ) != 0)
@@ -720,7 +774,7 @@ class Reactor {
     static final class TimeoutHandler implements Delayed, Runnable {
 
         /** The task to notify upon timeout. */
-        private final PendingOperation task;
+        private final AsyncOp<?> task;
 
         /**
          * The absolute deadline, in milliseconds, since the epoch.
@@ -737,7 +791,7 @@ class Reactor {
          * @param timeout the timeout
          * @param unit the unit of the timout
          */
-        TimeoutHandler(PendingOperation task, long timeout, TimeUnit unit) {
+        TimeoutHandler(AsyncOp<?> task, long timeout, TimeUnit unit) {
             this.task = task;
             this.deadlineMillis =
                 unit.toMillis(timeout) + System.currentTimeMillis();
