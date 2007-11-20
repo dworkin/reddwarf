c38cfbf448758edeae6988c3f4553923ce19006f
tb97327 <tb97327@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2007-11-20 15:58:29 +0000

    - Checkpoint.

--- analysis/server/j2se/com/sun/sgs/analysis/tasks/MapPutTask.java
+++ analysis/server/j2se/com/sun/sgs/analysis/tasks/MapPutTask.java
@@ -1,6 +1,7 @@
 package com.sun.sgs.analysis.task;
 
 import com.sun.sgs.app.AppContext;
+import com.sun.sgs.app.DataManager;
 import com.sun.sgs.app.ManagedObject;
 import com.sun.sgs.app.ManagedReference;
 import com.sun.sgs.app.Task;
@@ -8,7 +9,9 @@ import java.io.Serializable;
 import java.util.Map;
 import java.util.Random;
 
-/** A task that puts a random integer into a map and reschedules itself. */
+/**
+ * A task that puts a random integer into a map and reschedules itself a
+ * specified number of times. */
 public class MapPutTask implements ManagedObject, Serializable, Task {
 
     /** The version of the serialized form. */
@@ -20,13 +23,21 @@ public class MapPutTask implements ManagedObject, Serializable, Task {
     /** A reference to the map. */
     private final ManagedReference mapRef;
 
+    /** A reference to the object to notify when done. */
+    private final ManagedReference schedulerRef;
+
+    /** The remaining number of times to run. */
+    private int remaining;
+
     /** Creates an instance with the specified map. */
-    public MapPutTask(Map map) {
+    public MapPutTask(Map map, ScheduleMapPutsTask scheduler, int count) {
 	if (map == null) {
 	    throw new NullPointerException("The map must not be null");
 	}
-	mapRef = AppContext.getDataManager().createReference(
-	    (ManagedObject) map);
+	DataManager dataManager = AppContext.getDataManager();
+	mapRef = dataManager.createReference((ManagedObject) map);
+	schedulerRef = dataManager.createReference(scheduler);
+	remaining = count;
     }
 
     /** Puts a random integer into the map and reschedules this task. */
@@ -35,6 +46,11 @@ public class MapPutTask implements ManagedObject, Serializable, Task {
 	Map<Object, Object> map = mapRef.get(Map.class);
 	int i = random.nextInt();
 	map.put(i, i);
-	AppContext.getTaskManager().scheduleTask(this);
+	remaining--;
+	if (remaining == 0) {
+	    schedulerRef.get(ScheduleMapPutsTask.class).taskDone();
+	} else {
+	    AppContext.getTaskManager().scheduleTask(this);
+	}
     }
 }
