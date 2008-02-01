b1f1da6082eb06294031113b87e34788a6644eba
jm199043 <jm199043@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2008-02-01 15:55:37 +0000

    Backout an experimental linger-on-close implementation for now.

--- src/shared/j2se/com/sun/sgs/impl/io/SocketConnection.java
+++ src/shared/j2se/com/sun/sgs/impl/io/SocketConnection.java
@@ -20,17 +20,12 @@
 package com.sun.sgs.impl.io;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.mina.common.ByteBuffer;
-import org.apache.mina.common.IoFuture;
-import org.apache.mina.common.IoFutureListener;
 import org.apache.mina.common.IoSession;
 import org.apache.mina.common.TransportType;
-import org.apache.mina.common.WriteFuture;
 import org.apache.mina.transport.socket.nio.SocketSessionConfig;
 
 import com.sun.sgs.impl.sharedutil.LoggerWrapper;
@@ -42,9 +37,7 @@ import com.sun.sgs.io.ConnectionListener;
  * MINA framework.  It uses a {@link IoSession MINA IoSession} to handle the
  * IO transport.
  */
-public class SocketConnection
-    implements Connection, FilterListener, IoFutureListener
-{
+public class SocketConnection implements Connection, FilterListener {
 
     /** The logger for this class. */
     private static final LoggerWrapper logger =
@@ -58,22 +51,6 @@ public class SocketConnection
 
     /** The {@link IoSession} for this {@code Connection}. */
     private final IoSession session;
-
-    private final AtomicBoolean closed = new AtomicBoolean(false);
-
-    private final AtomicReference<IoFuture> lastWriteFuture =
-        new AtomicReference<IoFuture>();
-
-    private static boolean lingerOnClose;
-
-    static {
-        try {
-            lingerOnClose =
-                Boolean.valueOf(System.getProperty("com.sun.sgs.io.lingerOnClose", "false"));
-        } catch (RuntimeException e) {
-            lingerOnClose = false;
-        }
-    }
 
     /**
      * Construct a new SocketConnection with the given listener, filter, and
@@ -105,26 +82,21 @@ public class SocketConnection
      * {@inheritDoc}
      * <p>
      * This implementation prepends the length of the given byte array as
-     * a 2-byte {@code int} in network byte-order, and sends it out on
+     * a 4-byte {@code int} in network byte-order, and sends it out on
      * the underlying MINA {@code IoSession}.
      * 
      * @param message the data to send
      * @throws IOException if the session is not connected
      */
     public void sendBytes(byte[] message) throws IOException {
-        checkConnected();
-
-        // The filter does the actual work to prepend the length
-        filter.filterSend(this, message);
-    }
-
-    private void checkConnected() throws IOException {
-        if (!session.isConnected() || closed.get()) {
+        if (!session.isConnected()) {
             IOException ioe = new IOException(
                 "SocketConnection.close: session not connected");
             logger.logThrow(Level.FINE, ioe, ioe.getMessage());
-            throw ioe;
         }
+
+        // The filter does the actual work to prepend the length
+        filter.filterSend(this, message);
     }
 
     /**
@@ -132,17 +104,16 @@ public class SocketConnection
      * <p>
      * This implementation closes the underlying {@code IoSession}.
      *  
-     * @throws IOException if the session is not connected or has been closed
+     * @throws IOException if the session is not connected
      */
     public void close() throws IOException {
         logger.log(Level.FINER, "session = {0}", session);
-        if (! closed.compareAndSet(false, true)) {
-            checkConnected();
+        if (!session.isConnected()) {
+            IOException ioe = new IOException(
+                "SocketConnection.close: session not connected");
+            logger.logThrow(Level.FINE, ioe, ioe.getMessage());
         }
-
-        if (! lingerOnClose) {
-            session.close();
-        }
+        session.close();
     }
 
     // Implement FilterListener
@@ -166,22 +137,10 @@ public class SocketConnection
      */
     public void sendUnfiltered(ByteBuffer buf) {
         logger.log(Level.FINEST, "message = {0}", buf);
-        WriteFuture future = session.write(buf);
-        future.addListener(this);
-        lastWriteFuture.set(future);
+        session.write(buf);
     }
 
     // specific to SocketConnection
-
-    @Override
-    public void operationComplete(IoFuture future) {
-        if (lastWriteFuture.compareAndSet(future, null)) {
-            // If this was the last write and we want to close, do so.
-            if (closed.get() && session.isConnected()) {
-                session.close();
-            }
-        }
-    }
 
     /**
      * Returns the {@code ConnectionListener} for this connection. 
