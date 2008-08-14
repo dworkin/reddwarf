ac346bb4440a6bbd266aee09e826cae97900e2f8
jimwaldo <jimwaldo@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2008-08-14 17:50:31 +0000

    Added tests for sgs_id functions in id_test.c
    Repaired tests for sgs_ids and byte arrays in message_test.c

--- test/id_test.c
+++ test/id_test.c
@@ -35,71 +35,74 @@
  */
 
 #include "sgs/id.h"
-#include "sgs/hex_utils.h"
 
-void test1() {
-    uint8_t bytes[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int i;
-    sgs_id* id;
-    
-    for (i = 0; i<200; i++){
-        id = sgs_id_create(bytes, (i+1)%10);
-        sgs_id_destroy(id);
-    }
-    printf("create and destroy finished\n");
-    
-    id = sgs_id_create(bytes, 3);
-    if (id == NULL) {
-        sgs_id_destroy(id);
-        perror("init()");
-    }
-    
-    printf("bytelen = %ld\n", sgs_id_get_byte_len(id));
-    memcmp(bytes, sgs_id_get_bytes(id), sgs_id_get_byte_len(id));
-    for (i = 0; i < 3; i++){
-        printf("%dth value of bytes is %d\n", i, bytes[i]);
-    }
-    sgs_id_destroy(id);
-}
-
-void test2() {
-    const char* hex = "0102";
-    uint8_t bytebuf[1024];
-    sgs_id* id;
-    int result;
-    size_t i;
-    
-    result = hextobytes(hex, bytebuf);
-    
-    if (result == -1) {
-        printf("Error: invalid ID string (%s).\n", hex);
-        return;
-    }
-    
-    printf("hextobytes() returned %d\n", result);
-    
-    if ((id = sgs_id_create(bytebuf, strlen(hex)/2)) == NULL) {
-        printf("Error: invalid ID string (%s).\n", hex);
-        return;
-    }
-    
-    printf("byte: { ");
-    
-    for (i=0; i < sgs_id_get_byte_len(id); i++) {
-        if (i > 0) printf(", ");
-        printf("%d", *(sgs_id_get_bytes(id) + i));
-    }
-    
-    printf(" }\n");
-    sgs_id_destroy(id);
-}
-
-/*
- * function: main()
+/* Taken from message_test.c (should really be put into a shared utility 
+ * module); generates a set of random bytes of random lengths between 1 and 20
+ * that can be used to create a set of ids
  */
+void generateIds(sgs_id **testIds){
+    int i, j, k, len;
+    uint8_t buffer[10];
+    long sourcebuf[3];
+    
+    for (i = 0; i < 10; i++){
+        len = random()%10;
+        len++;
+        for (k = 0; k < 3; k++){
+            sourcebuf[k] = random();
+        }
+        memcpy(buffer, sourcebuf, len);
+        *(testIds + i) = sgs_id_create(buffer, len);
+    }
+}
+
+
 int main(int argc, char *argv[]) {
-    printf("bytes test:\n");
-    test1();
-    printf("\nhex test:\n");
-    test2();
+    sgs_id *testIds[10];
+    sgs_id *cloneIds[10];
+    int i, j, numBad, numBad1;
+    
+    numBad = 0;
+    numBad1 = 0;
+    
+    for (i = 0; i < 20; i++){
+        generateIds(testIds);
+        for (j = 0; j < 10; j++){
+            cloneIds[j] = sgs_id_duplicate(testIds[j]);
+        }
+        /* test to make sure the clones are identical to the original*/
+        for (j = 0; j< 10; j++){
+            if (0 != sgs_id_compare(testIds[j], cloneIds[j])){
+                numBad++;
+            }
+        }
+        if (numBad != 0){
+            printf("%d failures of equality on clones\n", numBad);
+       } 
+            
+        /*Test to make sure that compare doesn't always say they are 
+         *the same
+         */
+        for (j= 0; j < 10; j++){
+            if (0 == sgs_id_compare(testIds[j], cloneIds[(j+1)%10])){
+                numBad1++;
+            }
+        }
+        if (numBad1 != 0){
+            printf("%d false positive equality in ids\n", numBad);
+        }
+            
+        for (j = 0; j < 10; j++){
+            sgs_id_destroy(testIds[j]);
+            sgs_id_destroy(cloneIds[j]);
+        }
+    }
+    if ((numBad == 0)&&(numBad1 == 0)){
+        printf("Id tests passed\n");
+    } else {
+        printf("Id tests failed with %d failures\n", numBad + numBad1);
+    }
+    
+    return (numBad + numBad1);
+    
 }
