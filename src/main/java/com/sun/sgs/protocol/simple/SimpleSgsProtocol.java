a8fc2212b0dee00bebf3d45855fb02b3f5a12035
jimwaldo <jimwaldo@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2008-09-29 17:13:46 +0000

    More changes in adding a more complete specification to the wire protocol

--- src/main/java/com/sun/sgs/protocol/simple/SimpleSgsProtocol.java
+++ src/main/java/com/sun/sgs/protocol/simple/SimpleSgsProtocol.java
@@ -131,14 +131,14 @@ public interface SimpleSgsProtocol {
      * The reconnectionKey is an opaque reference that can be held by the client
      * for use in case the client is disconnected and wishes to reconnect to the
      * server with the same identity using a {@link #RECONNECT_REQUEST}. 
-     * <p>
+     * <br>
      * The behavior of a server receiving this message from a client that is 
      * already logged in is determined by the property com.sun.sgs.impl.service.session.allow.new.login.
      * If the value of this property is false (which is the default value) the
      * second login request is ignored. If the value is true, the second login
      * request will cause the currently active login to be replaced by the new
      * request.
-     * <p>
+     * <br>
      * This message should only be received by a client; if received by a server 
      * the client sending the message will be disconnected.
      */
@@ -154,9 +154,9 @@ public interface SimpleSgsProtocol {
      * <li> (String) reason
      * </ul>
      * This message indicates a failure of the login process initiated by a 
-     * LOGIN_REQUEST. The reason for the failure is encoded in the String returned
-     * as the reason; the interpretation of the reason is application specific.
-     * <p>
+     * {@link #LOGIN_REQUEST}. The reason for the failure is encoded in the returned
+     * string; the interpretation of the reason is application specific.
+     * <br>
      * This message should only be received by a client; if received by a server 
      * the client sending the message will be disconnected.
      */
@@ -179,7 +179,7 @@ public interface SimpleSgsProtocol {
      * indicated by the hostname and port in the payload. The client should 
      * then attempt to log in to the machine to which it has been redirected
      * by sending a {@link #LOGIN_REQUEST} to that machine.
-     * <p>
+     * <br>
      * This message should only be received by a client; if received by a server 
      * the client sending the message will be disconnected.
      */
@@ -195,6 +195,19 @@ public interface SimpleSgsProtocol {
      * <li> (byte)      protocol version
      * <li> (ByteArray) reconnectionKey
      * </ul>
+     * This message requests that the client be reconnected to an existing 
+     * login session with the server. The reconnectionKey must be the same that
+     * was sent to the client by the server as part of the {@link #LOGIN_SUCCESS}
+     * for the session to which reconnection is requested. Support for this message,
+     * and the length of time after a disconnect that a server will allow 
+     * reconnect, may vary from server to server. If the message is received by
+     * the server when the server state indicates that the client is currently 
+     * logged in with a valid session, the server will respond with a {@link 
+     * #RECONNECT_SUCCESS} message, and will continue with the current client
+     * session.
+     * <br>
+     * <b>Note: this message is not currently supported by the Project Darkstar
+     * Server</b>
      */
     final byte RECONNECT_REQUEST = 0x20;
 
@@ -208,6 +221,17 @@ public interface SimpleSgsProtocol {
      * <ul>
      * <li> (ByteArray) reconnectionKey
      * </ul>
+     * Indicates that a {@link #RECONNECT_REQUEST} has been successful. The 
+     * message will include a reconnect key that can be used in subsequent 
+     * reconnect requests from the client. Reciept of this message indicates that
+     * the client session has been re-established in the state it was in when the
+     * client was disconnected from the server.
+     * <br>
+     * This message should only be received by a client; if received by a server 
+     * the client sending the message will be disconnected.
+     * <br>
+     * <b>Note: this message is not currently supported by the Project Darkstar
+     * Server</b>
      */
     final byte RECONNECT_SUCCESS = 0x21;
 
@@ -221,6 +245,18 @@ public interface SimpleSgsProtocol {
      * <ul>
      * <li> (String) reason
      * </ul>
+     * This response indicates that a reconnect request could not be honored by
+     * the server. This could have been because of an invalid reconnect key, or 
+     * because too much time had elapsed between the client disconnection and 
+     * the reconnect request (which, in turn, caused the server to discard
+     * the client state). The string returned details the reason for the
+     * denial of reconnection.
+     * <br>
+     * This message should only be received by a client; if received by a server 
+     * the client sending the message will be disconnected.
+     * <br>
+     * <b>Note: this message is not currently supported by the Project Darkstar
+     * Server</b>
      */
     final byte RECONNECT_FAILURE = 0x22;
 
@@ -236,10 +272,13 @@ public interface SimpleSgsProtocol {
      * <ul>
      * <li> (ByteArray) message
      * </ul>
-     * <p>
+     * This message allows information to be sent between the client and the
+     * server. The content of the message is application dependent, and the
+     * mechanisms for constructing and parsing these messages is an application-level
+     * task. 
+     * <br>
      * If this message is sent by a client to a server and the client is not
-     * currently logged in to the server, the message will be ignored. The
-     * parsing of the message by the recipient is an application-level task.
+     * currently logged in to the server, the message will be ignored. 
      */
     final byte SESSION_MESSAGE = 0x30;
 
@@ -250,12 +289,12 @@ public interface SimpleSgsProtocol {
      * Opcode: {@code 0x40}
      * <br>
      * No payload.
-     * <p>
+     * <br>
      * This message will cause the client to be logged out of the server, and
      * the connection to be closed. Membership in any channels by the client will 
      * be dropped. Any message (other than {@link #LOGIN_REQUEST} sent by the
-     * client will be ignored, and any message will need to be sent on a 
-     * new connection to the server.
+     * client after sending this message will be ignored, and any message will 
+     * need to be sent on a new connection to the server.
      */
     final byte LOGOUT_REQUEST = 0x40;
 
@@ -265,7 +304,13 @@ public interface SimpleSgsProtocol {
      * Opcode: {@code 0x41}
      * <br>
      * No payload.
-      * <p>
+      * <br>
+     * This message is sent from the server to the client to indicate that a
+     * {@link #LOGOUT_REQUEST} has been received and that the client has been
+     * logged out of the current session. On receipt of this message, the client
+     * should shut down any networking resources that are used to communicate
+     * with the server.
+     * <br>
      * This message should only be received by a client; if received by a server 
      * the client sending the message will be disconnected.
     */
@@ -282,11 +327,18 @@ public interface SimpleSgsProtocol {
      * <li> (String) channel name
      * <li> (ByteArray) channel ID
      * </ul>
+     * This message is sent from the server to the client to indicate that the 
+     * client has been added to the channel identified by the name and channel ID
+     * contained in the message. The client should establish a way of receiving 
+     * messages that will be sent on the channel.
+     * <br>
+     * This message should only be received by a client; if received by a server 
+     * the client sending the message will be disconnected.
      */
     final byte CHANNEL_JOIN = 0x50;
 
     /**
-     * Channel leave.  Server notifying a client that it has left a channel.
+     * Channel leave.  Server notifying a client that the client has left a channel.
      * <br>
      * Opcode: {@code 0x51}
      * <br>
@@ -294,12 +346,18 @@ public interface SimpleSgsProtocol {
      * <ul>
      * <li> (ByteArray) channel ID
      * </ul>
+     * This message is sent from the server to the client indicating to the client
+     * that it has been removed from the channel with the indicated channel ID.
+     * The client can no longer send messages on the channel, and any objects
+     * that are currently listening for messages on the channel can be removed.
      */
     final byte CHANNEL_LEAVE = 0x51;
     
     /**
      * Channel message.  May be sent by the client or the server.
-     * Maximum length is {@value #MAX_PAYLOAD_LENGTH} bytes.
+     * Maximum length is {@value #MAX_PAYLOAD_LENGTH} bytes minus the channel ID
+     * size plus two bytes (the size of the unsigned short indicating the 
+     * channel Id size).
      * Larger messages require fragmentation and reassembly above
      * this protocol layer.
      * <br>
@@ -311,6 +369,10 @@ public interface SimpleSgsProtocol {
      * <li> (ByteArray) channel ID
      * <li> (ByteArray) message
      * </ul>
+     * This message will initiate the channel sending logic on the server, 
+     * requesting that the indicated message content be sent to all of the 
+     * members of the indicated channel. If the client sending the request
+     * is not a member of the channel, 
      */
     final byte CHANNEL_MESSAGE = 0x52;
 }
