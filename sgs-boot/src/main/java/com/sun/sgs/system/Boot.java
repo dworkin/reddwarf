7381f805da9205939d1ad82aea8dfd9a621fd17e
owenkellett <owenkellett@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2008-10-14 22:16:27 +0000

    preliminary bootstrapper to replace platform specific shell scripts and batch scripts

---
+++ sgs-boot/src/main/java/com/sun/sgs/system/Boot.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2007-2008 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sun.sgs.system;
+
+import java.net.URL;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Bootstraps and launches a Project Darkstar server
+ */
+public class Boot {
+    private static Logger logger = Logger.getLogger(Boot.class.getName());
+
+    public static void main(String[] args) {
+        //load properties from configuration file
+        Properties properties = new Properties();
+        try {
+            URL sgs_boot = ClassLoader.getSystemClassLoader().
+                    getResource(BootEnvironment.SGS_BOOT);
+            properties.load(sgs_boot.openStream());
+        } catch(Exception e) {
+            logger.log(Level.SEVERE, "Unable to load initial configuration", e);
+            System.exit(1);
+        }
+        
+        //ensure that SGS_HOME is set
+        String sgs_home = properties.getProperty(BootEnvironment.SGS_HOME);
+        if(sgs_home == null) {
+            logger.log(Level.SEVERE, BootEnvironment.SGS_HOME +
+                       " is not specified.");
+            System.exit(1);
+        }
+        
+        //load defaults for any missing properties
+        if(properties.getProperty(BootEnvironment.SGS_DEPLOY) == null) {
+            properties.setProperty(BootEnvironment.SGS_DEPLOY,
+                                   BootEnvironment.DEFAULT_SGS_DEPLOY);
+        }
+        if(properties.getProperty(BootEnvironment.SGS_LOGGING) == null) {
+            properties.setProperty(BootEnvironment.SGS_LOGGING,
+                                   BootEnvironment.DEFAULT_SGS_LOGGING);
+        }
+        if(properties.getProperty(BootEnvironment.SGS_LOGGING) == null) {
+            properties.setProperty(BootEnvironment.SGS_PROPERTIES,
+                                   BootEnvironment.DEFAULT_SGS_PROPERTIES);
+        }
+        
+        //autodetect BDB libraries if necessary
+        if(properties.getProperty(BootEnvironment.BDB_NATIVES) == null) {
+            String family = System.getProperty("os.family");
+            String name = System.getProperty("os.name");
+            String arch = System.getProperty("os.arch");
+
+            String bdb = null;
+            if (name.equals("Linux") && arch.equals("i386"))
+                bdb = BootEnvironment.DEFAULT_BDB_LINUX_X86;
+            else if(name.equals("Linux") && (arch.equals("x86_64") || arch.equals("amd64")))
+                bdb = BootEnvironment.DEFAULT_BDB_LINUX_X86_64;
+            else if(family.equals("mac") && (arch.equals("i386") || arch.equals("x86_64")))
+                bdb = BootEnvironment.DEFAULT_BDB_MACOSX_X86;
+            else if(family.equals("mac") && arch.equals("ppc"))
+                bdb = BootEnvironment.DEFAULT_BDB_MACOSX_PPC;
+            else if(name.equals("SunOS") && arch.equals("sparc"))
+                bdb = BootEnvironment.DEFAULT_BDB_SOLARIS_SPARC;
+            else if(name.equals("SunOS") && arch.equals("x86"))
+                bdb = BootEnvironment.DEFAULT_BDB_SOLARIS_X86;
+            else if(family.equals("windows"))
+                bdb = BootEnvironment.DEFAULT_BDB_WIN32_X86;
+            else {
+                logger.log(Level.SEVERE, "Unsupported platform: \n" +
+                           "Family: " + family + "\n" +
+                           "Name: " + name + "\n" +
+                           "Arch: " + arch);
+                System.exit(1);
+            }
+                
+            properties.setProperty(BootEnvironment.BDB_NATIVES, bdb);
+        }
+        
+        
+        System.out.print(properties);
+    }
+    
+}
---
+++ sgs-boot/src/main/java/com/sun/sgs/system/BootEnvironment.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2007-2008 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sun.sgs.system;
+
+/**
+ * Defines environment variables used by the bootstrapper
+ * to locate and configure the necessary environment pieces
+ * to launch a Project Darkstar Server
+ */
+public class BootEnvironment {
+    
+    /**
+     * Name of the properties file to locate and retrieve properties
+     * for the environment
+     */
+    public static final String SGS_BOOT = "sgs-boot.properties";
+
+    /**
+     * Denotes the installation directory for the Project Darkstar server
+     */
+    public static final String SGS_HOME = "SGS_HOME";
+    
+    /**
+     * The directory where deployed applications should place jar files
+     * and application properties files.  This directory is relative to
+     * {@link SGS_HOME}
+     */
+    public static final String SGS_DEPLOY = "SGS_DEPLOY";
+    
+    /**
+     * The properties file used to configure the Project Darkstar kernel.
+     * This file should be combined with the application's properties
+     * file and fed to the Project Darkstar Kernel.  It's location is
+     * relative to the {@link SGS_HOME}
+     */
+    public static final String SGS_PROPERTIES = "SGS_PROPERTIES";
+    
+    /**
+     * The logging properties file for the Project Darkstar server.
+     */
+    public static final String SGS_LOGGING = "SGS_LOGGING";
+    
+    /**
+     * The location of the Berkeley DB natives to include as part
+     * of the java.library.path
+     */
+    public static final String BDB_NATIVES = "BDB_NATIVES";
+    
+    /**
+     * Location of the JDK to use when booting up the Kernel
+     */
+    public static final String JAVA_HOME = "JAVA_HOME";
+    
+    /**
+     * Command line arguments for the JVM
+     */
+    public static final String JAVA_OPTS = "JAVA_OPTS";
+    
+    
+    public static final String DEFAULT_SGS_DEPLOY = "deploy/";
+    public static final String DEFAULT_SGS_PROPERTIES = "conf/sgs-server.properties";
+    public static final String DEFAULT_SGS_LOGGING = "conf/sgs-logging.properties";
+    
+    
+    public static final String DEFAULT_BDB_ROOT = "lib/bdb-natives/";
+    public static final String DEFAULT_BDB_LINUX_X86 = 
+            DEFAULT_BDB_ROOT + "linux-x86/";
+    public static final String DEFAULT_BDB_LINUX_X86_64 = 
+            DEFAULT_BDB_ROOT + "linux-x86_64/";
+    public static final String DEFAULT_BDB_MACOSX_X86 =
+            DEFAULT_BDB_ROOT + "macosx-x86/";
+    public static final String DEFAULT_BDB_MACOSX_PPC = 
+            DEFAULT_BDB_ROOT + "macosx-ppc/";
+    public static final String DEFAULT_BDB_SOLARIS_SPARC = 
+            DEFAULT_BDB_ROOT + "solaris-sparc/";
+    public static final String DEFAULT_BDB_SOLARIS_X86 = 
+            DEFAULT_BDB_ROOT + "solaris-x86/";
+    public static final String DEFAULT_BDB_WIN32_X86 = 
+            DEFAULT_BDB_ROOT + "win32-x86/";
+    
+}
