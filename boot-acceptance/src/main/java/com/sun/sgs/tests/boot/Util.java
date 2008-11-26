8ff55897fc3dc2c39ad005e3faa10e009fb3d0e2
owenkellett <owenkellett@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2008-11-26 13:34:29 +0000

    assembly

--- boot-acceptance/src/main/java/com/sun/sgs/tests/boot/Util.java
+++ boot-acceptance/src/main/java/com/sun/sgs/tests/boot/Util.java
@@ -23,11 +23,19 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipEntry;
 import java.util.Enumeration;
 import java.io.File;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.OutputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  *
  */
 class Util {
+    
+    private static final int BUFFER = 1024;
 
     /**
      * Unzips the {@code ZipFile} file into the given directory.
@@ -35,11 +43,38 @@ class Util {
      * @param file the file to unzip
      * @param directory the directory to extract the contents into
      */
-    public static void unzip(ZipFile file, File directory) {
+    public static void unzip(ZipFile file, File directory)  
+            throws IOException {
         for(Enumeration<? extends ZipEntry> e = file.entries(); 
                 e.hasMoreElements();) {
             ZipEntry z = e.nextElement();
-            System.out.println(z.getName());
+            
+            String dirName = directory.getAbsolutePath();
+            if(!dirName.endsWith(File.separator)) {
+                dirName += File.separator;
+            }
+            File entry = new File(dirName + z.getName());
+            
+            if(z.isDirectory()) {
+                entry.mkdirs();
+            } else {
+                InputStream is = null;
+                OutputStream os = null;
+                try {
+                    is = new BufferedInputStream(file.getInputStream(z));
+                    os = new BufferedOutputStream(new FileOutputStream(entry));
+
+                    byte[] buffer = new byte[BUFFER];
+                    int bytes = 0;
+                    while ((bytes = is.read(buffer, 0, BUFFER)) != -1) {
+                        os.write(buffer, 0, bytes);
+                    }
+                } finally {
+                    os.flush();
+                    os.close();
+                    is.close();
+                }
+            } 
         }
     }
 }
