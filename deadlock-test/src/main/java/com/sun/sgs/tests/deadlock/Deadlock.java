58ba5c4818024bce7a95f8bb7e166b59764bbec9
owenkellett <owenkellett@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-02-04 19:50:37 +0000

    updates per Tim's comments

--- deadlock-test/src/main/java/com/sun/sgs/tests/deadlock/Deadlock.java
+++ deadlock-test/src/main/java/com/sun/sgs/tests/deadlock/Deadlock.java
@@ -1,20 +1,33 @@
 /*
- * Copyright 2007-2008 Sun Microsystems, Inc.
+ * Copyright (c) 2007-2009, Sun Microsystems, Inc.
  *
- * This file is part of Project Darkstar Server.
+ * All rights reserved.
  *
- * Project Darkstar Server is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation and
- * distributed hereunder to you.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * Project Darkstar Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Sun Microsystems, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.sun.sgs.tests.deadlock;
@@ -41,17 +54,16 @@ import com.sun.sgs.app.AppContext;
  * to acquire an exclusive write lock one one of the integers, while also
  * acquiring a read lock on all of the other integers.
  * <p>
- * A single, self-replicating {@link ReportTask} is also scheduled which tracks
+ * A single {@link ReportTask} is also scheduled to run once per 
+ * second which tracks
  * and reports on the successful update rate of a single {@code ManagedInteger}
  * in the list of {@code ManagedInteger}s.
  */
 public class Deadlock implements AppListener, Serializable {
     
-    private static String SIZE = "com.sun.sgs.tests.deadlock.size";
-    private static int DEFAULT_SIZE = 10;
+    private static final String SIZE = "com.sun.sgs.tests.deadlock.size";
+    private static final int DEFAULT_SIZE = 10;
     
-    private ManagedReference<ManagedInteger>[] integers;
-
     public void initialize(Properties props) {
         int size;
         String sizeProp = props.getProperty(SIZE);
@@ -61,8 +73,9 @@ public class Deadlock implements AppListener, Serializable {
             size = DEFAULT_SIZE;
         }
         
-        integers = new ManagedReference[size];
-        for(int i = 0; i < size; i++) {
+        ManagedReference<ManagedInteger>[] integers = 
+                new ManagedReference[size];
+        for (int i = 0; i < size; i++) {
             integers[i] = AppContext.getDataManager().createReference(
                     new ManagedInteger());
             AppContext.getTaskManager().scheduleTask(
