317ed6fdac50dfef7759c72fe31c79d9c5951b35
owenkellett <owenkellett@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-03-17 14:47:51 +0000

    additional scripts for pre and post release process

---
+++ sgs-release/deploy-release.sh
@@ -0,0 +1,35 @@
+#!/usr/xpg4/bin/sh -xe
+#
+# This script is used to automatically increment the version number of a project,
+# verify that the updates are correct, and then tag and deploy it to the
+# java.net maven repository.
+#
+# NOTE: This script is hardcoded to the Solaris /usr/xpg4/bin/sh shell
+#
+
+if [ $# -ne 6 ]; then
+    echo "Invalid number of arguments"
+    echo "Usage: $0 [url] [branchname] [tagname] [username] [password] [version]"
+    exit 1
+fi
+
+URL=$1
+BRANCH=$2
+TAG=$3
+USERNAME=$4
+PASSWORD=$5
+VERSION=$6
+
+# remove any old working copy
+rm -rf co
+
+# checkout the branch
+svn --non-interactive --username $USERNAME --password $PASSWORD co $URL/branches/$BRANCH co
+cd co
+
+# deploy to java.net
+mvn deploy -DskipTests
+
+# tag the branch
+svn --non-interactive --username $USERNAME --password $PASSWORD copy $URL/branches/$BRANCH $URL/tags/$TAG -m "tagging release $VERSION"
+
