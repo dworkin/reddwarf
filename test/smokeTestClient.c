a7c1ed3bf424bb03f029e82e992987cca771bcd1
jimwaldo <jimwaldo@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-04-20 11:31:14 +0000

    start of breaking apart smoketest into separate source files

--- test/smokeTestClient.c
+++ test/smokeTestClient.c
@@ -18,6 +18,7 @@
 #include "sgs/context.h"
 #include "sgs/session.h"
 #include "sgs/map.h"
+#include "testCallbacks.h"
 
 
 /*
@@ -28,72 +29,17 @@
  * over-ridden on the command line
  */
 #define DEFAULT_HOST "localhost"
-#define DEFAULT_PORT 1139;
+#define DEFAULT_PORT 1139
 
 /* Some global variables, declared to make life easier */
 
-static fd_set g_master_readset, g_master_writeset, g_master_exceptset;
-static int g_maxfd;
 static char *g_hostname = DEFAULT_HOST;
 static int g_port = DEFAULT_PORT;
 
-/*
- * register_fd_cb()
- * This callback is used in the throttling code; this will be
- * called when throttling is turned off (that is, when reading
- * from the file descriptor is re-enabled after being turned off
- * via a call to unregister_fd_cb)
- */
-void register_fd_cb(sgs_connection *conn, int fd, short events) {
-
-    if ((events & POLLIN) == POLLIN)
-        FD_SET(fd, &g_master_readset);
-
-    if ((events & POLLOUT) == POLLOUT)
-        FD_SET(fd, &g_master_writeset);
-
-    if ((events & POLLERR) == POLLERR)
-        FD_SET(fd, &g_master_exceptset);
-
-    if (fd > g_maxfd) g_maxfd = fd;
-}
-
-/*
- * unregister_fd_cb()
- * This callback is used in the throttling code; this will be
- * called when things are getting clogged to turn off the
- * reading of the indicated file descriptor.
- */
-void unregister_fd_cb(sgs_connection *conn, int fd, short events) {
-    int i, new_max;
-
-    if ((events & POLLIN) == POLLIN)
-        FD_CLR(fd, &g_master_readset);
-
-    if ((events & POLLOUT) == POLLOUT)
-        FD_CLR(fd, &g_master_writeset);
-
-    if ((events & POLLERR) == POLLERR)
-        FD_CLR(fd, &g_master_exceptset);
-
-    /** Check if a new max-fd needs to be calculated. */
-    if (fd == g_maxfd) {
-        new_max = 0;
-
-        for (i=0; i <= g_maxfd; i++) {
-            if (FD_ISSET(i, &g_master_readset) ||
-                FD_ISSET(i, &g_master_writeset) ||
-                FD_ISSET(i, &g_master_exceptset))
-                new_max = i;
-        }
-
-        g_maxfd = new_max;
-    }
-}
 
 void getCommandArgs(int count, char *args[]){
     int c;
-    while ((c = getopt(count, args, "h:p:u"))){
+    while ((c = getopt(count, args, "h:p:u")) != -1){
         switch (c){
             case 'h': /* set the global hostname variable*/
                 g_hostname = optarg;
@@ -105,14 +51,19 @@ void getCommandArgs(int count, char *args[]){
 
             case 'u' : /*print usage*/
                 printf("Usage: %s [-h HOST] [-p PORT] [-u] \n", args[0]);
-                printf (""-h specify remote host for server (default %s)\n", DEFAULT_HOST);
+                printf ("-h specify remote host for server (default %s)\n", DEFAULT_HOST);
                 printf("-p specify port for server (default %d)\n", DEFAULT_PORT);
-                printf("-u Print usage\n"); -
+                printf("-u Print usage\n");
                  break;
         }
     }
 }
+
+
 int main(int argc, char** argv) {
+    sgs_context *context;
+    sgs_connection *connection;
+    sgs_connection *session;
 
     FD_ZERO(&g_master_readset);
     FD_ZERO(&g_master_writeset);
@@ -120,7 +71,15 @@ int main(int argc, char** argv) {
 
     getCommandArgs(argc, argv);
 
-    printf("parsed command line; hostname = %s, port = %d", g_hostname, g_port);
+    printf("parsed command line; hostname = %s, port = %d\n", g_hostname, g_port);
+
+    context = sgs_ctx_create(g_hostname, g_port, register_fd_cb, unregister_fd_cb);
+    if (context == NULL) {
+        printf("error in context create\n");
+        exit(1);
+    }
+
+
     
     return (EXIT_SUCCESS);
 }
