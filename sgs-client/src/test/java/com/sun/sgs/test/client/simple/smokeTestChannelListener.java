66c3e83328fecbdac3720cf4ec1da6446d425847
jimwaldo <jimwaldo@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-06-03 15:49:25 +0000

    Interim commit

--- sgs-client/src/test/java/com/sun/sgs/test/client/simple/smokeTestChannelListener.java
+++ sgs-client/src/test/java/com/sun/sgs/test/client/simple/smokeTestChannelListener.java
@@ -8,8 +8,11 @@ package com.sun.sgs.test.client.simple;
 import com.sun.sgs.client.ClientChannel;
 import com.sun.sgs.client.ClientChannelListener;
 import com.sun.sgs.client.simple.SimpleClient;
+import com.sun.sgs.impl.sharedutil.LoggerWrapper;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,27 +20,42 @@ import java.nio.ByteBuffer;
  */
 public class smokeTestChannelListener implements ClientChannelListener{
     private SimpleClient client;
+    private boolean receiveMsgPass, leftChannelPass;
+
+    private static final LoggerWrapper logger =
+            new LoggerWrapper(Logger.getLogger(smokeTestChannelListener.class.getName()));
     
     public smokeTestChannelListener(SimpleClient withClient){
         client = withClient;
+        receiveMsgPass = leftChannelPass = false;
     }
 
     public void receivedMessage(ClientChannel channel, ByteBuffer message){
         String sendMsg = "receivedChannelMessage" + channel.getName() + "" + message.toString();
+        receiveMsgPass = true;
         try {
                     channel.send(ByteBuffer.wrap(sendMsg.getBytes()));
         } catch (IOException e){
-            System.out.println("Unable to send response to received channel message on channel "
+            logger.log(Level.WARNING, "Unable to send response to received channel message on channel "
                     + channel.getName());
         }
     }
 
     public void leftChannel(ClientChannel channel){
         String msg = "leftChannel:" + channel.getName();
+        leftChannelPass = true;
         try {
             client.send(ByteBuffer.wrap(msg.getBytes()));
         } catch (IOException e){
-            System.out.println("unable to send channel leave confirmation");
+            logger.log(Level.WARNING, "unable to send channel leave confirmation");
         }
+    }
+
+    boolean getReceiveStatus(){
+        return receiveMsgPass;
+    }
+
+    boolean getChannelLeftStatus(){
+        return leftChannelPass;
     }
 }
