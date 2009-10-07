0ba57ca761d25f52ce0129d7a36c321b26d02499
owenkellett <owenkellett@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-10-07 19:59:16 +0000

    refactored ContinuePolicy into the internal api

--- sgs-server-internal-api/src/main/java/com/sun/sgs/service/task/ContinuePolicy.java
+++ sgs-server-internal-api/src/main/java/com/sun/sgs/service/task/ContinuePolicy.java
@@ -15,24 +15,30 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the LICENSE file that accompanied
+ * this code.
  */
 
-package com.sun.sgs.impl.service.task;
+package com.sun.sgs.service.task;
 
 import com.sun.sgs.app.TaskManager;
+import com.sun.sgs.app.TransactionException;
 import com.sun.sgs.kernel.ComponentRegistry;
+import com.sun.sgs.service.TaskService;
 import com.sun.sgs.service.TransactionProxy;
 import java.util.Properties;
 
 /**
- * This interface is used to define a continuation policy for the
- * {@link TaskServiceImpl} when the {@link TaskServiceImpl#shouldContinue()}
- * method is called.
+ * This interface is used to define a pluggable continuation policy for
+ * {@link TaskService} implementations when the
+ * {@link TaskService#shouldContinue()} method is called.
  *
  * All implementations must define a constructor of the form
  * ({@link Properties}, {@link ComponentRegistry}, {@link TransactionProxy})
  */
-interface ContinuePolicy {
+public interface ContinuePolicy {
 
     /**
      * Returns {@code true} if the currently running task should do more work
