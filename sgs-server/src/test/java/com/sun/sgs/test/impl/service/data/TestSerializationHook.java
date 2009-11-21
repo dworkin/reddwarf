6da1c31da4cd2d9ae453c02b0e0163d1c422efb3
luontola <luontola@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2009-11-21 17:18:45 +0000

    Added copyright headers

--- sgs-server/src/test/java/com/sun/sgs/test/impl/service/data/TestSerializationHook.java
+++ sgs-server/src/test/java/com/sun/sgs/test/impl/service/data/TestSerializationHook.java
@@ -1,7 +1,27 @@
-package com.sun.sgs.impl.service.data;
+/*
+ * Copyright 2007-2009 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sun.sgs.test.impl.service.data;
 
 import com.sun.sgs.app.ManagedObject;
 import com.sun.sgs.impl.hook.*;
+import com.sun.sgs.impl.service.data.*;
 import org.junit.*;
 
 import java.io.*;
