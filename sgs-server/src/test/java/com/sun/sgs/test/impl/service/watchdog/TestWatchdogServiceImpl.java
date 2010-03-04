9eb28bc807e575ae0ae3176e114d4e4832bbeee0
jimwaldo <jimwaldo@fcbef2f2-e9bc-6140-a6ae-93e2c1843930>
2010-03-04 16:43:54 +0000

    updated copyright notice

--- sgs-server/src/test/java/com/sun/sgs/test/impl/service/watchdog/TestWatchdogServiceImpl.java
+++ sgs-server/src/test/java/com/sun/sgs/test/impl/service/watchdog/TestWatchdogServiceImpl.java
@@ -1,22 +1,11 @@
 /*
- * Copyright 2007-2010 Sun Microsystems, Inc.
+ * This material is distributed under the GNU General Public License
+ * Version 2. You may review the terms of this license at
+ * http://www.gnu.org/licenses/gpl-2.0.html 
  *
- * This file is part of Project Darkstar Server.
+ * Copyright (c) 2007-2010, Oracle and/or its affiliates.
  *
- * Project Darkstar Server is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation and
- * distributed hereunder to you.
- *
- * Project Darkstar Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * --
+ * All rights reserved.
  */
 
 package com.sun.sgs.test.impl.service.watchdog;
